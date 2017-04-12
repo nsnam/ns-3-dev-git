934fc1e3e9e350b0a905e732c4046897f237caf0
Kushal Gowda <kushalg1697@gmail.com>
2017-04-12 21:49:47 +0530

    Update sfq-queue-disc.h

--- src/traffic-control/model/sfq-queue-disc.h
+++ src/traffic-control/model/sfq-queue-disc.h
@@ -1,8 +1,58 @@
 #include "ns3/queue-disc.h"
 #include "ns3/object-factory.h"
-#include  <list>
-#include  <map>
+#include <list>
+#include <map>
 
-namespace ns3{
-  
+namespace ns3 {
+
+	class SfqQueueFlow : public QueueDiscClass {
+	
+	public:
+
+	private:
+		
+	}
+
+	class SfqQueueDisc : public QueueDisc {
+	public:
+	
+		/*
+			Get the type ID		
+		*/
+		static TypeId GetTypeId (void);
+
+		/*
+			SfqQueueDisc Constructor
+		*/	
+		SfqQueueDisc ();
+
+		virtual ~SfqQueueDisc ();
+
+		/*
+			Set the quantum Value.
+		*/		
+		/*
+			quantum: The number of bytes each queue gets to dequeue on each round of the scheduling algorithm
+		*/
+		void SetQuantum (uint32_t quantum);
+		
+		/*
+			getter function (returns quantum value)
+		*/
+		uint32_t GetQuantum (void) const;
+		
+	private:
+		virtual bool DoEnqueue (Ptr<QueueDiscItem> item);
+		virtual Ptr<QueueDiscItem> DoDequeue (void);
+		virtual void InitializeParams (void);
+
+
+	}
+
+
+
+
 }
+
+
+
