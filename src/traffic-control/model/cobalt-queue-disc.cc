f8ed784721a7341978ae73c3a8f0e33e255f2db4
Daipu <jendaipoupalmei@gmail.com>
2019-03-23 18:44:12 +0530

    traffic-control: Add COBALT QueueDisc

---
+++ src/traffic-control/model/cobalt-queue-disc.cc
@@ -0,0 +1,505 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2019 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Cobalt, the CoDel - BLUE - Alternate Queueing discipline
+ * Based on linux code.
+ *
+ * Ported to ns-3 by: Vignesh Kannan <vignesh2496@gmail.com>
+ *                    Harsh Lara <harshapplefan@gmail.com>
+ *                    Jendaipou Palmei <jendaipoupalmei@gmail.com>
+ *                    Shefali Gupta <shefaligups11@gmail.com>
+ *                    Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+#include "ns3/log.h"
+#include "ns3/enum.h"
+#include "ns3/uinteger.h"
+#include "ns3/abort.h"
+#include "cobalt-queue-disc.h"
+#include "ns3/object-factory.h"
+#include "ns3/drop-tail-queue.h"
+#include "ns3/net-device-queue-interface.h"
+#include <climits>
+
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("CobaltQueueDisc");
+
+NS_OBJECT_ENSURE_REGISTERED (CobaltQueueDisc);
+
+TypeId CobaltQueueDisc::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::CobaltQueueDisc")
+    .SetParent<QueueDisc> ()
+    .SetGroupName ("TrafficControl")
+    .AddConstructor<CobaltQueueDisc> ()
+    .AddAttribute ("MaxSize",
+                   "The maximum number of packets/bytes accepted by this queue disc.",
+                   QueueSizeValue (QueueSize (QueueSizeUnit::BYTES, 1500 * DEFAULT_COBALT_LIMIT)),
+                   MakeQueueSizeAccessor (&QueueDisc::SetMaxSize,
+                                          &QueueDisc::GetMaxSize),
+                   MakeQueueSizeChecker ())
+    .AddAttribute ("MinBytes",
+                   "The Cobalt algorithm minbytes parameter.",
+                   UintegerValue (1500),
+                   MakeUintegerAccessor (&CobaltQueueDisc::m_minBytes),
+                   MakeUintegerChecker<uint32_t> ())
+    .AddAttribute ("Interval",
+                   "The Cobalt algorithm interval",
+                   StringValue ("100ms"),
+                   MakeTimeAccessor (&CobaltQueueDisc::m_interval),
+                   MakeTimeChecker ())
+    .AddAttribute ("Target",
+                   "The Cobalt algorithm target queue delay",
+                   StringValue ("5ms"),
+                   MakeTimeAccessor (&CobaltQueueDisc::m_target),
+                   MakeTimeChecker ())
+    .AddAttribute ("UseEcn",
+                   "True to use ECN (packets are marked instead of being dropped)",
+                   BooleanValue (false),
+                   MakeBooleanAccessor (&CobaltQueueDisc::m_useEcn),
+                   MakeBooleanChecker ())
+    .AddAttribute ("Pdrop",
+                   "Marking Probability",
+                   DoubleValue (0),
+                   MakeDoubleAccessor (&CobaltQueueDisc::m_Pdrop),
+                   MakeDoubleChecker<double> ())
+    .AddAttribute ("Increment",
+                   "Pdrop increment value",
+                   DoubleValue (1. / 256),
+                   MakeDoubleAccessor (&CobaltQueueDisc::m_increment),
+                   MakeDoubleChecker<double> ())
+    .AddAttribute ("Decrement",
+                   "Pdrop decrement Value",
+                   DoubleValue (1. / 4096),
+                   MakeDoubleAccessor (&CobaltQueueDisc::m_decrement),
+                   MakeDoubleChecker<double> ())
+    .AddTraceSource ("Count",
+                     "Cobalt count",
+                     MakeTraceSourceAccessor (&CobaltQueueDisc::m_count),
+                     "ns3::TracedValueCallback::Uint32")
+    .AddTraceSource ("DropState",
+                     "Dropping state",
+                     MakeTraceSourceAccessor (&CobaltQueueDisc::m_dropping),
+                     "ns3::TracedValueCallback::Bool")
+    .AddTraceSource ("DropNext",
+                     "Time until next packet drop",
+                     MakeTraceSourceAccessor (&CobaltQueueDisc::m_dropNext),
+                     "ns3::TracedValueCallback::Uint32")
+  ;
+
+  return tid;
+}
+
+/**
+ * Performs a reciprocal divide, similar to the
+ * Linux kernel reciprocal_divide function
+ * \param A numerator
+ * \param R reciprocal of the denominator B
+ * \return the value of A/B
+ */
+/* borrowed from the linux kernel */
+static inline uint32_t ReciprocalDivide (uint32_t A, uint32_t R)
+{
+  return (uint32_t)(((uint64_t)A * R) >> 32);
+}
+
+double min (double x, double y)
+{
+  return (x < y) ? x : y;
+}
+
+double max (double x, double y)
+{
+  return (x > y) ? x : y;
+}
+
+/**
+ * Returns the current time translated in CoDel time representation
+ * \return the current time
+ */
+static int64_t CoDelGetTime (void)
+{
+  Time time = Simulator::Now ();
+  int64_t ns = time.GetNanoSeconds ();
+
+  return ns;
+}
+
+CobaltQueueDisc::CobaltQueueDisc ()
+  : QueueDisc ()
+{
+  NS_LOG_FUNCTION (this);
+  InitializeParams ();
+  m_uv = CreateObject<UniformRandomVariable> ();
+}
+
+double CobaltQueueDisc::GetPdrop ()
+{
+  return m_Pdrop;
+}
+
+CobaltQueueDisc::~CobaltQueueDisc ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+int64_t
+CobaltQueueDisc::AssignStreams (int64_t stream)
+{
+  NS_LOG_FUNCTION (this << stream);
+  m_uv->SetStream (stream);
+  return 1;
+}
+
+void
+CobaltQueueDisc::InitializeParams (void)
+{
+  // Cobalt parameters
+  NS_LOG_FUNCTION (this);
+  m_recInvSqrtCache[0] = ~0;
+  CacheInit ();
+  m_count = 0;
+  m_dropping = false;
+  m_recInvSqrt = ~0U;
+  m_lastUpdateTimeBlue = 0;
+  m_dropNext = 0;
+}
+
+bool
+CobaltQueueDisc::CoDelTimeAfter (int64_t a, int64_t b)
+{
+  return  ((int64_t)(a) - (int64_t)(b) > 0);
+}
+
+bool
+CobaltQueueDisc::CoDelTimeAfterEq (int64_t a, int64_t b)
+{
+  return ((int64_t)(a) - (int64_t)(b) >= 0);
+}
+
+int64_t
+CobaltQueueDisc::Time2CoDel (Time t)
+{
+  return (t.GetNanoSeconds ());
+}
+
+Time
+CobaltQueueDisc::GetTarget (void)
+{
+  return m_target;
+}
+
+Time
+CobaltQueueDisc::GetInterval (void)
+{
+  return m_interval;
+}
+
+int64_t
+CobaltQueueDisc::GetDropNext (void)
+{
+  return m_dropNext;
+}
+
+void
+CobaltQueueDisc::NewtonStep (void)
+{
+  NS_LOG_FUNCTION (this);
+  uint32_t invsqrt = ((uint32_t) m_recInvSqrt);
+  uint32_t invsqrt2 = ((uint64_t) invsqrt * invsqrt) >> 32;
+  uint64_t val = (3ll << 32) - ((uint64_t) m_count * invsqrt2);
+
+  val >>= 2; /* avoid overflow */
+  val = (val * invsqrt) >> (32 - 2 + 1);
+  m_recInvSqrt = val;
+}
+
+void
+CobaltQueueDisc::CacheInit (void)
+{
+  m_recInvSqrt = ~0U;
+  m_recInvSqrtCache[0] = m_recInvSqrt;
+
+  for (m_count = 1; m_count < (uint32_t)(REC_INV_SQRT_CACHE); m_count++)
+    {
+      NewtonStep ();
+      NewtonStep ();
+      NewtonStep ();
+      NewtonStep ();
+      m_recInvSqrtCache[m_count] = m_recInvSqrt;
+    }
+}
+
+void
+CobaltQueueDisc::InvSqrt (void)
+{
+  if (m_count < (uint32_t)REC_INV_SQRT_CACHE)
+    {
+      m_recInvSqrt = m_recInvSqrtCache[m_count];
+    }
+  else
+    {
+      NewtonStep ();
+    }
+}
+
+int64_t
+CobaltQueueDisc::ControlLaw (int64_t t)
+{
+  NS_LOG_FUNCTION (this);
+  return t + ReciprocalDivide (Time2CoDel (m_interval), m_recInvSqrt);
+}
+
+void
+CobaltQueueDisc::DoDispose (void)
+{
+  NS_LOG_FUNCTION (this);
+  m_uv = 0;
+  QueueDisc::DoDispose ();
+}
+
+Ptr<const QueueDiscItem>
+CobaltQueueDisc::DoPeek (void)
+{
+  NS_LOG_FUNCTION (this);
+  if (GetInternalQueue (0)->IsEmpty ())
+    {
+      NS_LOG_LOGIC ("Queue empty");
+      return 0;
+    }
+
+  Ptr<const QueueDiscItem> item = GetInternalQueue (0)->Peek ();
+
+  NS_LOG_LOGIC ("Number packets " << GetInternalQueue (0)->GetNPackets ());
+  NS_LOG_LOGIC ("Number bytes " << GetInternalQueue (0)->GetNBytes ());
+
+  return item;
+}
+
+bool
+CobaltQueueDisc::CheckConfig (void)
+{
+  NS_LOG_FUNCTION (this);
+  if (GetNQueueDiscClasses () > 0)
+    {
+      NS_LOG_ERROR ("CobaltQueueDisc cannot have classes");
+      return false;
+    }
+
+  if (GetNPacketFilters () > 0)
+    {
+      NS_LOG_ERROR ("CobaltQueueDisc cannot have packet filters");
+      return false;
+    }
+
+  if (GetNInternalQueues () == 0)
+    {
+
+      AddInternalQueue (CreateObjectWithAttributes<DropTailQueue<QueueDiscItem> >
+                          ("MaxSize", QueueSizeValue (GetMaxSize ())));
+    }
+
+
+  if (GetNInternalQueues () != 1)
+    {
+      NS_LOG_ERROR ("CobaltQueueDisc needs 1 internal queue");
+      return false;
+    }
+  return true;
+}
+
+bool
+CobaltQueueDisc::DoEnqueue (Ptr<QueueDiscItem> item)
+{
+  NS_LOG_FUNCTION (this << item);
+  Ptr<Packet> p = item->GetPacket ();
+  if (GetCurrentSize () + item > GetMaxSize ())
+    {
+      NS_LOG_LOGIC ("Queue full -- dropping pkt");
+      int64_t now = CoDelGetTime ();
+      // Call this to update Blue's drop probability
+      CobaltQueueFull (now);
+      DropBeforeEnqueue (item, OVERLIMIT_DROP);
+      return false;
+    }
+
+  bool retval = GetInternalQueue (0)->Enqueue (item);
+
+  // If Queue::Enqueue fails, QueueDisc::Drop is called by the internal queue
+  // because QueueDisc::AddInternalQueue sets the drop callback
+
+  NS_LOG_LOGIC ("Number packets " << GetInternalQueue (0)->GetNPackets ());
+  NS_LOG_LOGIC ("Number bytes " << GetInternalQueue (0)->GetNBytes ());
+
+  return retval;
+}
+
+Ptr<QueueDiscItem>
+CobaltQueueDisc::DoDequeue (void)
+{
+  NS_LOG_FUNCTION (this);
+
+  while (1)
+    {
+      Ptr<QueueDiscItem> item = GetInternalQueue (0)->Dequeue ();
+      if (!item)
+        {
+          // Leave dropping state when queue is empty (derived from Codel)
+          m_dropping = false;
+          NS_LOG_LOGIC ("Queue empty");
+          int64_t now = CoDelGetTime ();
+          // Call this to update Blue's drop probability
+          CobaltQueueEmpty (now);
+          return 0;
+        }
+
+      int64_t now = CoDelGetTime ();
+
+      NS_LOG_LOGIC ("Popped " << item);
+      NS_LOG_LOGIC ("Number packets remaining " << GetInternalQueue (0)->GetNPackets ());
+      NS_LOG_LOGIC ("Number bytes remaining " << GetInternalQueue (0)->GetNBytes ());
+
+      // Determine if item should be dropped
+      // ECN marking happens inside this function, so it need not be done here
+      bool drop = CobaltShouldDrop (item, now);
+
+      if (drop)
+        {
+          DropAfterDequeue (item, TARGET_EXCEEDED_DROP);
+        }
+      else
+        {
+          return item;
+        }
+    }
+}
+
+// Call this when a packet had to be dropped due to queue overflow.
+void CobaltQueueDisc::CobaltQueueFull (int64_t now)
+{
+  NS_LOG_FUNCTION (this);
+  NS_LOG_LOGIC ("Outside IF block");
+  if (CoDelTimeAfter ((now - m_lastUpdateTimeBlue), Time2CoDel (m_target)))
+    {
+      NS_LOG_LOGIC ("inside IF block");
+      m_Pdrop = min (m_Pdrop + m_increment, (double)1.0);
+      m_lastUpdateTimeBlue = now;
+    }
+  m_dropping = true;
+  m_dropNext = now;
+  if (!m_count)
+    {
+      m_count = 1;
+    }
+}
+
+// Call this when the queue was serviced but turned out to be empty.
+void CobaltQueueDisc::CobaltQueueEmpty (int64_t now)
+{
+  NS_LOG_FUNCTION (this);
+  if (m_Pdrop && CoDelTimeAfter ((now - m_lastUpdateTimeBlue), Time2CoDel (m_target)))
+    {
+      m_Pdrop = max (m_Pdrop - m_decrement, (double)0.0);
+      m_lastUpdateTimeBlue = now;
+    }
+  m_dropping = false;
+
+  if (m_count && CoDelTimeAfterEq ((now - m_dropNext), 0))
+    {
+      m_count--;
+      InvSqrt ();
+      m_dropNext = ControlLaw (m_dropNext);
+    }
+}
+
+// Determines if Cobalt should drop the packet
+bool CobaltQueueDisc::CobaltShouldDrop (Ptr<QueueDiscItem> item, int64_t now)
+{
+  NS_LOG_FUNCTION (this);
+  bool drop = false;
+
+  /* Simplified Codel implementation */
+  Time delta = Simulator::Now () - item->GetTimeStamp ();
+  NS_LOG_INFO ("Sojourn time " << delta.GetSeconds ());
+  int64_t sojournTime = Time2CoDel (delta);
+  int64_t schedule = now - m_dropNext;
+  bool over_target = CoDelTimeAfter (sojournTime, Time2CoDel (m_target));
+  bool next_due = m_count && schedule >= 0;
+
+  if (over_target)
+    {
+      if (!m_dropping)
+        {
+          m_dropping = true;
+          m_dropNext = ControlLaw (now);
+        }
+      if (!m_count)
+        {
+          m_count = 1;
+        }
+    }
+  else if (m_dropping)
+    {
+      m_dropping = false;
+    }
+
+  if (next_due && m_dropping)
+    {
+      /* Check for marking possibility only if BLUE decides NOT to drop. */
+      /* Check if router and packet, both have ECN enabled. Only if this is true, mark the packet. */
+      drop = !(m_useEcn && Mark (item, FORCED_MARK));
+
+      m_count = max (m_count, m_count + 1);
+
+      InvSqrt ();
+      m_dropNext = ControlLaw (m_dropNext);
+      schedule = now - m_dropNext;
+    }
+  else
+    {
+      while (next_due)
+        {
+          m_count--;
+          InvSqrt ();
+          m_dropNext = ControlLaw (m_dropNext);
+          schedule = now - m_dropNext;
+          next_due = m_count && schedule >= 0;
+        }
+    }
+  /* Simple BLUE implementation. Lack of ECN is deliberate. */
+  if (m_Pdrop)
+    {
+      double u = m_uv->GetValue ();
+      drop = drop | (u < m_Pdrop);
+    }
+
+  /* Overload the drop_next field as an activity timeout */
+  if (!m_count)
+    {
+      m_dropNext = now + Time2CoDel (m_interval);
+    }
+  else if (schedule > 0 && !drop)
+    {
+      m_dropNext = now;
+    }
+
+  return drop;
+}
+
+} // namespace ns3
---
+++ src/traffic-control/model/cobalt-queue-disc.h
@@ -0,0 +1,247 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2019 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Cobalt, the CoDel - BLUE - Alternate Queueing discipline
+ * Based on linux code.
+ *
+ * Ported to ns-3 by: Vignesh Kannan <vignesh2496@gmail.com>
+ *                    Harsh Lara <harshapplefan@gmail.com>
+ *                    Jendaipou Palmei <jendaipoupalmei@gmail.com>
+ *                    Shefali Gupta <shefaligups11@gmail.com>
+ *                    Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ *
+ */
+
+#ifndef COBALT_H
+#define COBALT_H
+
+#include "ns3/queue-disc.h"
+#include "ns3/nstime.h"
+#include "ns3/boolean.h"
+#include "ns3/data-rate.h"
+#include "ns3/simulator.h"
+#include "ns3/string.h"
+#include "ns3/traced-value.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/trace-source-accessor.h"
+
+namespace ns3 {
+
+#define REC_INV_SQRT_CACHE (16)
+#define DEFAULT_COBALT_LIMIT 1000
+
+class TraceContainer;
+
+/**
+ * \ingroup traffic-control
+ *
+ * \brief Cobalt packet queue disc
+ *
+ * Cobalt uses CoDel and BLUE algorithms in parallel, in order
+ * to obtain the best features of each. CoDel is excellent on flows
+ * which respond to congestion signals in a TCP-like way. BLUE is far
+ * more effective on unresponsive flows.
+ */
+class CobaltQueueDisc : public QueueDisc
+{
+public:
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
+
+  /**
+   * \brief CobaltQueueDisc Constructor
+   *
+   * Create a Cobalt queue disc
+   */
+  CobaltQueueDisc ();
+
+  /**
+   * \brief Destructor
+   *
+   * Destructor
+   */
+  virtual ~CobaltQueueDisc ();
+
+  /**
+   * \brief Get the target queue delay
+   *
+   * \returns The target queue delay
+   */
+  Time GetTarget (void);
+
+  /**
+   * \brief Get the interval
+   *
+   * \returns The interval
+   */
+  Time GetInterval (void);
+
+  /**
+   * \brief Get the time for next packet drop while in the dropping state
+   *
+   * \returns The time for next packet drop
+   */
+  int64_t GetDropNext (void);
+
+  static constexpr const char* TARGET_EXCEEDED_DROP = "Target exceeded drop";  //!< Sojourn time above target
+  static constexpr const char* OVERLIMIT_DROP = "Overlimit drop";  //!< Overlimit dropped packet
+  static constexpr const char* FORCED_MARK = "forcedMark";  //!< forced marks by Codel on ECN-enabled
+
+  /**
+   * \brief Get the drop probability of Blue
+   *
+   * \returns The current value of Blue's drop probability
+   */
+  double GetPdrop ();
+
+  /**
+   * Assign a fixed random variable stream number to the random variables
+   * used by this model.  Return the number of streams (possibly zero) that
+   * have been assigned.
+   *
+   * \param stream first stream index to use
+   * \return the number of stream indices assigned by this model
+   */
+  int64_t AssignStreams (int64_t stream);
+
+  /**
+   * Return the unsigned 32-bit integer representation of the input Time
+   * object. Units are microseconds
+   * @param t the input Time Object
+   * @return the unsigned 32-bit integer representation
+   */
+  int64_t Time2CoDel (Time t);
+
+protected:
+  /**
+   * \brief Dispose of the object
+   */
+  virtual void DoDispose (void);
+
+private:
+  virtual bool DoEnqueue (Ptr<QueueDiscItem> item);
+  virtual Ptr<QueueDiscItem> DoDequeue (void);
+  virtual Ptr<const QueueDiscItem> DoPeek (void);
+  virtual bool CheckConfig (void);
+
+  /**
+    * \brief Initialize the queue parameters.
+    */
+  virtual void InitializeParams (void);
+
+  /**
+   * \brief Calculate the reciprocal square root of m_count by using Newton's method
+   *  http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Iterative_methods_for_reciprocal_square_roots
+   * m_recInvSqrt (new) = (m_recInvSqrt (old) / 2) * (3 - m_count * m_recInvSqrt^2)
+   */
+  void NewtonStep (void);
+
+  /**
+   * \brief Determine the time for next drop
+   * CoDel control law is t + m_interval/sqrt(m_count).
+   * Here, we use m_recInvSqrt calculated by Newton's method in NewtonStep() to avoid
+   * both sqrt() and divide operations
+   *
+   * \param t Current next drop time
+   * \returns The new next drop time:
+   */
+  int64_t ControlLaw (int64_t t);
+
+  void InvSqrt (void);
+
+  /**
+   * There is a big difference in timing between the accurate values placed in
+   * the cache and the approximations given by a single Newton step for small
+   * count values, particularly when stepping from count 1 to 2 or vice versa.
+   * Above 16, a single Newton step gives sufficient accuracy in either
+   * direction, given the precision stored.
+   *
+   * The magnitude of the error when stepping up to count 2 is such as to give
+   * the value that *should* have been produced at count 4.
+   */
+  void CacheInit (void);
+
+  /**
+   * Check if CoDel time a is successive to b
+   * @param a left operand
+   * @param b right operand
+   * @return true if a is greater than b
+   */
+
+  bool CoDelTimeAfter (int64_t a, int64_t b);
+
+  /**
+   * Check if CoDel time a is successive or equal to b
+   * @param a left operand
+   * @param b right operand
+   * @return true if a is greater than or equal to b
+   */
+  bool CoDelTimeAfterEq (int64_t a, int64_t b);
+
+  /**
+   * Called when the queue becomes full to alter the drop probabilities of Blue
+   */
+  void CobaltQueueFull (int64_t now);
+
+  /**
+   * Called when the queue becomes empty to alter the drop probabilities of Blue
+   */
+  void CobaltQueueEmpty (int64_t now);
+
+  /**
+   * Called to decide whether the current packet should be dropped based on decisions taken by Blue and Codel working parallely
+   * Returns true if the packet should be dropped, false otherwise
+   */
+  bool CobaltShouldDrop (Ptr<QueueDiscItem> item, int64_t now);
+
+  // Common to CoDel and Blue
+  // Maintained by Cobalt
+  Stats m_stats;                          //!< Cobalt statistics
+  // Supplied by user
+  uint32_t m_minBytes;                    //!< Minimum bytes in queue to allow a packet drop
+
+  // Codel parameters
+  // Maintained by Cobalt
+  TracedValue<uint32_t> m_count;          //!< Number of packets dropped since entering drop state
+  TracedValue<int64_t> m_dropNext;       //!< Time to drop next packet
+  TracedValue<bool> m_dropping;           //!< True if in dropping state
+  uint32_t m_recInvSqrt;                  //!< Reciprocal inverse square root
+  uint32_t m_recInvSqrtCache[REC_INV_SQRT_CACHE] = {0};   //!< Cache to maintain some initial values of InvSqrt
+
+  // Supplied by user
+  Time m_interval;                        //!< 100 ms sliding minimum time window width
+  Time m_target;                          //!< 5 ms target queue delay
+  bool m_useEcn;                          //!< True if ECN is used (packets are marked instead of being dropped)
+
+  // Blue parameters
+  // Maintained by Cobalt
+  Ptr<UniformRandomVariable> m_uv;        //!< Rng stream
+  uint32_t m_lastUpdateTimeBlue;          //!< Blue's last update time for drop probability
+
+  // Supplied by user
+  double m_increment;                     //!< increment value for marking probability
+  double m_decrement;                     //!< decrement value for marking probability
+  double m_Pdrop;                         //!< Drop Probability
+
+};
+
+} // namespace ns3
+
+#endif /* COBALT_H */
