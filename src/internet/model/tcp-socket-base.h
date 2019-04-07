4c461961c587998a3e3f3ea13897d6218f7e8c6c
Chakradhar malagundla <chakradharmalagundla@gmail.com>
2019-04-07 13:49:49 +0530

    Added new EcnMode EcnpTry in enum
    EcnpTry represents ECN+/TryOnce and works as defined in RFC 5562

--- src/internet/model/tcp-socket-base.h
+++ src/internet/model/tcp-socket-base.h
@@ -122,8 +122,7 @@ public:
  * the main TcpCongestionOps class. Switching between congestion algorithm is
  * now a matter of setting a pointer into the TcpSocketBase class. The idea
  * and the interfaces are inspired by the Linux operating system, and in
- * particular from the structure tcp_congestion_ops. The reference paper is
- * https://www.sciencedirect.com/science/article/pii/S1569190X15300939.
+ * particular from the structure tcp_congestion_ops.
  *
  * Transmission Control Block (TCB)
  * --------------------------------
@@ -209,8 +208,7 @@ public:
  * On the other side, inside TcpTxBuffer the received options (that contain
  * the SACK block) are processed and a particular data structure, called Scoreboard,
  * is filled. Please take a look at TcpTxBuffer and TcpRxBuffer documentation if
- * you need more information. The reference paper is
- * https://dl.acm.org/citation.cfm?id=3067666.
+ * you need more information.
  *
  */
 class TcpSocketBase : public TcpSocket
@@ -440,11 +438,17 @@ public:
    */
   void SetRecoveryAlgorithm (Ptr<TcpRecoveryOps> recovery);
 
+
+  //set ce flag to 1
+  void SetCE(Ptr<Packet> p);
+
+
   /**
    * \brief Mark ECT(0)
    *
    * \return TOS with ECT(0)
    */
+
   inline uint8_t MarkEcnEct0 (uint8_t tos) const
     {
       return ((tos & 0xfc) | 0x02);
@@ -479,14 +483,14 @@ public:
     {
       return tos & 0xfc;
     }
-
   /**
    * \brief ECN Modes
    */
   typedef enum
     {
       NoEcn = 0,   //!< ECN is not enabled.
-      ClassicEcn   //!< ECN functionality as described in RFC 3168.
+      ClassicEcn,   //!< ECN functionality as described in RFC 3168.
+      EcnpTry,     //!< ECN functionality as described in RFC 5562-ECN/TryOnce
     } EcnMode_t;
 
   /**
@@ -748,7 +752,7 @@ protected:
    *
    * \param flags the packet's flags
    */
-  virtual void SendEmptyPacket (uint8_t flags);
+  virtual void SendEmptyPacket (uint8_t flags,bool markect);
 
   /**
    * \brief Send reset and tear down this socket
@@ -1166,7 +1170,7 @@ protected:
    * \brief Add Tags for the Socket
    * \param p Packet
    */
-  void AddSocketTags (const Ptr<Packet> &p) const;
+  void AddSocketTags (const Ptr<Packet> &p,bool withEct=false) const;
 
 protected:
   // Counters and events
@@ -1189,6 +1193,8 @@ protected:
   uint32_t          m_synRetries   {0}; //!< Number of connection attempts
   uint32_t          m_dataRetrCount {0}; //!< Count of remaining data retransmission attempts
   uint32_t          m_dataRetries  {0}; //!< Number of data retransmission attempts
+
+  
 
   // Timeouts
   TracedValue<Time> m_rto     {Seconds (0.0)}; //!< Retransmit timeout
