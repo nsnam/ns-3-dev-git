81bcc67bb02c7cfebeaf22b13595ab4d86840ad5
ZorazeAli <zoraze.ali@cttc.es>
2019-04-24 17:30:35 +0200

    lte: Add Fraunhofer copyright for RLF

--- src/lte/model/lte-enb-rrc.h
+++ src/lte/model/lte-enb-rrc.h
@@ -1,6 +1,7 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2011, 2012 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ * Copyright (c) 2018 Fraunhofer ESK (RLF)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
--- src/lte/model/lte-ue-phy.h
+++ src/lte/model/lte-ue-phy.h
@@ -1,6 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2010 TELEMATICS LAB, DEE - Politecnico di Bari
+ * Copyright (c) 2018 Fraunhofer ESK (RLF)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
