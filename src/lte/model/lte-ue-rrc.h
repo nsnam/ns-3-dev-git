c05e3fc0684256ebedc96f5e8b9ef132cd01e44c
ZorazeAli <zoraze.ali@cttc.es>
2019-05-03 11:33:37 +0200

    lte: Update copyright for RLF extensions

--- src/lte/model/lte-ue-rrc.h
+++ src/lte/model/lte-ue-rrc.h
@@ -1,7 +1,7 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
- * Copyright (c) 2018 Fraunhofer ESK (RLF)
+ * Copyright (c) 2018 Fraunhofer ESK : RLF extensions
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
