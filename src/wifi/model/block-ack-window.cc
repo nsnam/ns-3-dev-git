333c58765a0d9df854a0ce8e7b35d4b25a19bb45
Stefano Avallone <stavallo@unina.it>
2019-08-28 18:51:22 +0200

    wifi: Add a BlockAckWindow class

---
+++ src/wifi/model/block-ack-window.cc
@@ -0,0 +1,95 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2019 Universita' degli Studi di Napoli Federico II
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Stefano Avallone <stavallo@unina.it>
+ */
+
+#include "ns3/log.h"
+#include "block-ack-window.h"
+#include "wifi-utils.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("BlockAckWindow");
+
+BlockAckWindow::BlockAckWindow ()
+  : m_winStart (0),
+    m_head (0)
+{
+}
+
+void
+BlockAckWindow::Init (uint16_t winStart, uint16_t winSize)
+{
+  NS_LOG_FUNCTION (this << winStart << winSize);
+  m_winStart = winStart;
+  m_window.assign (winSize, false);
+  m_head = 0;
+}
+
+void
+BlockAckWindow::Reset (uint16_t winStart)
+{
+  Init (winStart, m_window.size ());
+}
+
+uint16_t
+BlockAckWindow::GetWinStart (void) const
+{
+  return m_winStart;
+}
+
+uint16_t
+BlockAckWindow::GetWinEnd (void) const
+{
+  return (m_winStart + m_window.size () - 1) % SEQNO_SPACE_SIZE;
+}
+
+std::size_t
+BlockAckWindow::GetWinSize (void) const
+{
+  return m_window.size ();
+}
+
+std::vector<bool>::reference
+BlockAckWindow::At (std::size_t distance)
+{
+  NS_ASSERT (distance < m_window.size ());
+
+  return m_window.at ((m_head + distance) % m_window.size ());
+}
+
+void
+BlockAckWindow::Advance (std::size_t count)
+{
+  NS_LOG_FUNCTION (this << count);
+
+  if (count >= m_window.size ())
+    {
+      Reset ((m_winStart + count) % SEQNO_SPACE_SIZE);
+      return;
+    }
+
+  for (std::size_t i = 0; i < count; i++)
+    {
+      m_window[m_head] = false;
+      m_head = (m_head + 1) % m_window.size ();
+    }
+  m_winStart = (m_winStart + count) % SEQNO_SPACE_SIZE;
+}
+
+} //namespace ns3
---
+++ src/wifi/model/block-ack-window.h
@@ -0,0 +1,120 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2019 Universita' degli Studi di Napoli Federico II
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Stefano Avallone <stavallo@unina.it>
+ */
+
+#ifndef BLOCK_ACK_WINDOW_H
+#define BLOCK_ACK_WINDOW_H
+
+#include <vector>
+
+namespace ns3 {
+
+/**
+ * \ingroup wifi
+ * \brief Block Ack window
+ *
+ * This class provides the basic functionalities of a window sliding over a
+ * bitmap: accessing any element in the bitmap and moving the window forward
+ * a given number of positions. This class can be used to implement both
+ * an originator's window and a recipient's window.
+ *
+ * The window is implemented as a vector of bool and managed as a circular
+ * queue. The window is moved forward by advancing the head of the queue and
+ * clearing the elements that become part of the tail of the queue. Hence,
+ * no element is required to be shifted when the window moves forward.
+ *
+ * Example:
+ *
+ * |0|1|1|0|1|1|1|0|1|1|1|1|1|1|1|0|
+ *                      ^
+ *                      |
+ *                     HEAD
+ *
+ * After moving the window forward three positions:
+ *
+ * |0|1|1|0|1|1|1|0|1|1|0|0|0|1|1|0|
+ *                            ^
+ *                            |
+ *                           HEAD
+ */
+class BlockAckWindow
+{
+public:
+  /**
+   * Constructor
+   */
+  BlockAckWindow ();
+  /**
+   * Initialize the window with the given starting sequence number and size
+   *
+   * \param winStart the window start
+   * \param winSize the window size
+   */
+  void Init (uint16_t winStart, uint16_t winSize);
+  /**
+   * Reset the window by clearing all the elements and setting winstart to the
+   * given value.
+   *
+   * \param winStart the window start
+   */
+  void Reset (uint16_t winStart);
+  /**
+   * Get the current winstart value.
+   *
+   * \return the current winstart value
+   */
+  uint16_t GetWinStart (void) const;
+  /**
+   * Get the current winend value.
+   *
+   * \return the current winend value
+   */
+  uint16_t GetWinEnd (void) const;
+  /**
+   * Get the window size.
+   *
+   * \return the window size
+   */
+  std::size_t GetWinSize (void) const;
+  /**
+   * Get a reference to the element in the window having the given distance from
+   * the current winstart. Note that the given distance must be less than the
+   * window size.
+   *
+   * \param distance the given distance
+   * \return a reference to the element in the window having the given distance
+   *         from the current winstart
+   */
+  std::vector<bool>::reference At (std::size_t distance);
+  /**
+   * Advance the current winstart by the given number of positions.
+   *
+   * \param count the number of positions the current winstart must be advanced by
+   */
+  void Advance (std::size_t count);
+
+private:
+  uint16_t m_winStart;         ///< window start (sequence number)
+  std::vector<bool> m_window;  ///< window
+  std::size_t m_head;          ///< index of winstart in the vector
+};
+
+} //namespace ns3
+
+#endif /* BLOCK_ACK_WINDOW_H */
