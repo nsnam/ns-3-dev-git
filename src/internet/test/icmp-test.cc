69a30ad42c4a6fc457bf64b8215295cf9d46b39c
Tom Henderson <tomh@tomh.org>
2019-09-16 07:09:47 -0700

    Remove tabs and fix whitespace

--- src/internet/test/icmp-test.cc
+++ src/internet/test/icmp-test.cc
@@ -141,7 +141,7 @@ IcmpEchoReplyTestCase::DoSendData (Ptr<Socket> socket, Ipv4Address dst)
   Address realTo = InetSocketAddress (dst, 1234);
 
   NS_TEST_EXPECT_MSG_EQ (socket->SendTo (p, 0, realTo),
- 	                         (int) p->GetSize(), " Unable to send ICMP Echo Packet");
+                         (int) p->GetSize (), " Unable to send ICMP Echo Packet");
 
 }
 
@@ -151,7 +151,7 @@ IcmpEchoReplyTestCase::SendData (Ptr<Socket> socket, Ipv4Address dst)
 {
   m_receivedPacket = Create<Packet> ();
   Simulator::ScheduleWithContext (socket->GetNode ()->GetId (), Seconds (0),
-	                                  &IcmpEchoReplyTestCase::DoSendData, this, socket, dst);
+                                  &IcmpEchoReplyTestCase::DoSendData, this, socket, dst);
   Simulator::Run ();
 }
 
@@ -160,7 +160,7 @@ IcmpEchoReplyTestCase::ReceivePkt (Ptr <Socket> socket)
 {
   Address from;
   Ptr<Packet> p = socket->RecvFrom (0xffffffff, 0, from);
-  m_receivedPacket = p->Copy();
+  m_receivedPacket = p->Copy ();
 
   Ipv4Header ipv4;
   p->RemoveHeader (ipv4);
@@ -169,8 +169,8 @@ IcmpEchoReplyTestCase::ReceivePkt (Ptr <Socket> socket)
   Icmpv4Header icmp;
   p->RemoveHeader (icmp);
 
-  NS_TEST_EXPECT_MSG_EQ (icmp.GetType(), Icmpv4Header::ICMPV4_ECHO_REPLY,
-		                 " The received Packet is not a ICMPV4_ECHO_REPLY");
+  NS_TEST_EXPECT_MSG_EQ (icmp.GetType (), Icmpv4Header::ICMPV4_ECHO_REPLY,
+                         " The received Packet is not a ICMPV4_ECHO_REPLY");
 }
 
 
@@ -209,8 +209,8 @@ IcmpEchoReplyTestCase::DoRun ()
   NS_TEST_EXPECT_MSG_EQ (socket->Bind (src),0," Socket Binding failed");
 
   // Set a TTL big enough
-  socket->SetIpTtl(1);
-  SendData (socket, i.GetAddress(1,0));
+  socket->SetIpTtl (1);
+  SendData (socket, i.GetAddress (1,0));
 
   NS_TEST_EXPECT_MSG_EQ (m_receivedPacket->GetSize (), 28, " Unexpected ICMPV4_ECHO_REPLY packet size");
 
@@ -272,7 +272,7 @@ IcmpTimeExceedTestCase::DoSendData (Ptr<Socket> socket, Ipv4Address dst)
   Address realTo = InetSocketAddress (dst, 1234);
 
   NS_TEST_EXPECT_MSG_EQ (socket->SendTo (p, 0, realTo),
-							 (int) p->GetSize(), " Unable to send ICMP Echo Packet");
+                         (int) p->GetSize (), " Unable to send ICMP Echo Packet");
 }
 
 
@@ -281,7 +281,7 @@ IcmpTimeExceedTestCase::SendData (Ptr<Socket> socket, Ipv4Address dst)
 {
   m_receivedPacket = Create<Packet> ();
   Simulator::ScheduleWithContext (socket->GetNode ()->GetId (), Seconds (0),
-									  &IcmpTimeExceedTestCase::DoSendData, this, socket, dst);
+                                  &IcmpTimeExceedTestCase::DoSendData, this, socket, dst);
   Simulator::Run ();
 }
 
@@ -291,20 +291,20 @@ IcmpTimeExceedTestCase::ReceivePkt (Ptr<Socket> socket)
 {
   Address from;
   Ptr<Packet> p = socket->RecvFrom (0xffffffff, 0, from);
-  m_receivedPacket = p->Copy();
+  m_receivedPacket = p->Copy ();
 
   Ipv4Header ipv4;
   p->RemoveHeader (ipv4);
   NS_TEST_EXPECT_MSG_EQ (ipv4.GetProtocol (), 1,"The received packet is not an ICMP packet");
 
-  NS_TEST_EXPECT_MSG_EQ (ipv4.GetSource(),Ipv4Address("10.0.0.2"),
-		                 "ICMP Time Exceed Response should come from 10.0.0.2");
+  NS_TEST_EXPECT_MSG_EQ (ipv4.GetSource (),Ipv4Address ("10.0.0.2"),
+                         "ICMP Time Exceed Response should come from 10.0.0.2");
 
   Icmpv4Header icmp;
   p->RemoveHeader (icmp);
 
-  NS_TEST_EXPECT_MSG_EQ (icmp.GetType(), Icmpv4Header::ICMPV4_TIME_EXCEEDED,
-						 "The received packet is not a ICMPV4_TIME_EXCEEDED packet ");
+  NS_TEST_EXPECT_MSG_EQ (icmp.GetType (), Icmpv4Header::ICMPV4_TIME_EXCEEDED,
+                         "The received packet is not a ICMPV4_TIME_EXCEEDED packet ");
 }
 
 
@@ -312,35 +312,35 @@ void
 IcmpTimeExceedTestCase::DoRun ()
 {
   NodeContainer n, n0n1,n1n2;
-  n.Create(3);
-  n0n1.Add(n.Get(0));
-  n0n1.Add(n.Get(1));
-  n1n2.Add(n.Get(1));
-  n1n2.Add(n.Get(2));
+  n.Create (3);
+  n0n1.Add (n.Get (0));
+  n0n1.Add (n.Get (1));
+  n1n2.Add (n.Get (1));
+  n1n2.Add (n.Get (2));
 
   Ptr<SimpleChannel> channel = CreateObject <SimpleChannel> ();
   Ptr<SimpleChannel> channel2 = CreateObject <SimpleChannel> ();
 
   SimpleNetDeviceHelper simpleHelper;
-  simpleHelper.SetNetDevicePointToPointMode(true);
+  simpleHelper.SetNetDevicePointToPointMode (true);
 
   SimpleNetDeviceHelper simpleHelper2;
-  simpleHelper2.SetNetDevicePointToPointMode(true);
+  simpleHelper2.SetNetDevicePointToPointMode (true);
 
   NetDeviceContainer devices;
-  devices = simpleHelper.Install(n0n1,channel);
+  devices = simpleHelper.Install (n0n1,channel);
   NetDeviceContainer devices2;
-  devices2 = simpleHelper2.Install(n1n2,channel2);
+  devices2 = simpleHelper2.Install (n1n2,channel2);
 
   InternetStackHelper internet;
   internet.Install (n);
 
   Ipv4AddressHelper address;
-  address.SetBase("10.0.0.0","255.255.255.255");
-  Ipv4InterfaceContainer i = address.Assign(devices);
+  address.SetBase ("10.0.0.0","255.255.255.255");
+  Ipv4InterfaceContainer i = address.Assign (devices);
 
-  address.SetBase("10.0.1.0","255.255.255.255");
-  Ipv4InterfaceContainer i2 = address.Assign(devices2);
+  address.SetBase ("10.0.1.0","255.255.255.255");
+  Ipv4InterfaceContainer i2 = address.Assign (devices2);
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
@@ -354,8 +354,8 @@ IcmpTimeExceedTestCase::DoRun ()
 
 
   // The ttl is not big enough , causing an ICMP Time Exceeded response
-  socket->SetIpTtl(1);
-  SendData (socket, i2.GetAddress(1,0));
+  socket->SetIpTtl (1);
+  SendData (socket, i2.GetAddress (1,0));
 
   NS_TEST_EXPECT_MSG_EQ (m_receivedPacket->GetSize (), 56, " Unexpected ICMP Time Exceed Response packet size");
 
@@ -403,9 +403,9 @@ void
 IcmpV6EchoReplyTestCase::DoSendData (Ptr<Socket> socket, Ipv6Address dst)
 {
   Ptr<Packet> p = Create<Packet> ();
-  Icmpv6Echo echo(1);
-  echo.SetSeq(1);
-  echo.SetId(0XB1ED);
+  Icmpv6Echo echo (1);
+  echo.SetSeq (1);
+  echo.SetId (0XB1ED);
   p->AddHeader (echo);
 
   Icmpv6Header header;
@@ -416,7 +416,7 @@ IcmpV6EchoReplyTestCase::DoSendData (Ptr<Socket> socket, Ipv6Address dst)
   Address realTo = Inet6SocketAddress (dst, 1234);
 
   NS_TEST_EXPECT_MSG_EQ (socket->SendTo (p, 0, realTo),
- 	                         (int) p->GetSize(), " Unable to send ICMP Echo Packet");
+                         (int) p->GetSize (), " Unable to send ICMP Echo Packet");
 
 }
 
@@ -426,7 +426,7 @@ IcmpV6EchoReplyTestCase::SendData (Ptr<Socket> socket, Ipv6Address dst)
 {
   m_receivedPacket = Create<Packet> ();
   Simulator::ScheduleWithContext (socket->GetNode ()->GetId (), Seconds (0),
-	                                  &IcmpV6EchoReplyTestCase::DoSendData, this, socket, dst);
+                                  &IcmpV6EchoReplyTestCase::DoSendData, this, socket, dst);
   Simulator::Run ();
 }
 
@@ -435,26 +435,26 @@ IcmpV6EchoReplyTestCase::ReceivePkt (Ptr <Socket> socket)
 {
   Address from;
   Ptr<Packet> p = socket->RecvFrom (from);
-  m_receivedPacket = p->Copy();
+  m_receivedPacket = p->Copy ();
 
   if (Inet6SocketAddress::IsMatchingType (from))
     {
-	  Ipv6Header ipv6;
-	  p->RemoveHeader (ipv6);
+      Ipv6Header ipv6;
+      p->RemoveHeader (ipv6);
 
-	  NS_TEST_EXPECT_MSG_EQ (ipv6.GetNextHeader(),
-			                 Ipv6Header::IPV6_ICMPV6,
-							 "The received Packet is not an ICMPV6 packet");
-	  Icmpv6Header icmpv6;
-	  p->RemoveHeader (icmpv6);
+      NS_TEST_EXPECT_MSG_EQ (ipv6.GetNextHeader (),
+                             Ipv6Header::IPV6_ICMPV6,
+                             "The received Packet is not an ICMPV6 packet");
+      Icmpv6Header icmpv6;
+      p->RemoveHeader (icmpv6);
 
-	  // Ignore the neighbor discovery (ICMPV6_ND) packets
-	  if (!(((int)icmpv6.GetType() >= 133) && ((int)icmpv6.GetType() <= 137)))
-	    {
-		  NS_TEST_EXPECT_MSG_EQ ((int) icmpv6.GetType(),
-				                 Icmpv6Header::ICMPV6_ECHO_REPLY,
-		 			             "The received Packet is not a ICMPV6_ECHO_REPLY");
-	    }
+      // Ignore the neighbor discovery (ICMPV6_ND) packets
+      if (!(((int)icmpv6.GetType () >= 133) && ((int)icmpv6.GetType () <= 137)))
+        {
+          NS_TEST_EXPECT_MSG_EQ ((int) icmpv6.GetType (),
+                                 Icmpv6Header::ICMPV6_ECHO_REPLY,
+                                 "The received Packet is not a ICMPV6_ECHO_REPLY");
+        }
     }
 }
 
@@ -492,13 +492,13 @@ IcmpV6EchoReplyTestCase::DoRun ()
   socket->SetAttribute ("Protocol", UintegerValue (Ipv6Header::IPV6_ICMPV6));
   socket->SetRecvCallback (MakeCallback (&IcmpV6EchoReplyTestCase::ReceivePkt, this));
 
-  Inet6SocketAddress src = Inet6SocketAddress (Ipv6Address::GetAny(), 0);
+  Inet6SocketAddress src = Inet6SocketAddress (Ipv6Address::GetAny (), 0);
   NS_TEST_EXPECT_MSG_EQ (socket->Bind (src),0," SocketV6 Binding failed");
 
   // Set a TTL big enough
-  socket->SetIpTtl(1);
+  socket->SetIpTtl (1);
 
-  SendData (socket, i.GetAddress(1,1));
+  SendData (socket, i.GetAddress (1,1));
 
   NS_TEST_EXPECT_MSG_EQ (m_receivedPacket->GetSize (), 72, " Unexpected ICMPV6_ECHO_REPLY packet size");
 
@@ -546,9 +546,9 @@ void
 IcmpV6TimeExceedTestCase::DoSendData (Ptr<Socket> socket, Ipv6Address dst)
 {
   Ptr<Packet> p = Create<Packet> ();
-  Icmpv6Echo echo(1);
-  echo.SetSeq(1);
-  echo.SetId(0XB1ED);
+  Icmpv6Echo echo (1);
+  echo.SetSeq (1);
+  echo.SetId (0XB1ED);
   p->AddHeader (echo);
 
   Icmpv6Header header;
@@ -569,7 +569,7 @@ IcmpV6TimeExceedTestCase::SendData (Ptr<Socket> socket, Ipv6Address dst)
 {
   m_receivedPacket = Create<Packet> ();
   Simulator::ScheduleWithContext (socket->GetNode ()->GetId (), Seconds (0),
-	                                  &IcmpV6TimeExceedTestCase::DoSendData, this, socket, dst);
+                                  &IcmpV6TimeExceedTestCase::DoSendData, this, socket, dst);
   Simulator::Run ();
 }
 
@@ -578,28 +578,28 @@ IcmpV6TimeExceedTestCase::ReceivePkt (Ptr <Socket> socket)
 {
   Address from;
   Ptr<Packet> p = socket->RecvFrom (from);
-  m_receivedPacket = p->Copy();
+  m_receivedPacket = p->Copy ();
 
   if (Inet6SocketAddress::IsMatchingType (from))
     {
-	  Ipv6Header ipv6;
-	  p->RemoveHeader (ipv6);
+      Ipv6Header ipv6;
+      p->RemoveHeader (ipv6);
 
 
-	  NS_TEST_EXPECT_MSG_EQ (ipv6.GetNextHeader(),
-			                 Ipv6Header::IPV6_ICMPV6,
-							 "The received Packet is not an ICMPV6 packet");
+      NS_TEST_EXPECT_MSG_EQ (ipv6.GetNextHeader (),
+                             Ipv6Header::IPV6_ICMPV6,
+                             "The received Packet is not an ICMPV6 packet");
 
-	  Icmpv6Header icmpv6;
-	  p->RemoveHeader (icmpv6);
+      Icmpv6Header icmpv6;
+      p->RemoveHeader (icmpv6);
 
-	  // Ignore the neighbor discovery (ICMPV6_ND) packets
-	  if (!(((int)icmpv6.GetType() >= 133) && ((int)icmpv6.GetType() <= 137)))
-	    {
-		  NS_TEST_EXPECT_MSG_EQ ((int) icmpv6.GetType(),
-				                 Icmpv6Header::ICMPV6_ERROR_TIME_EXCEEDED,
-		 			             "The received Packet is not a ICMPV6_ERROR_TIME_EXCEEDED");
-	    }
+      // Ignore the neighbor discovery (ICMPV6_ND) packets
+      if (!(((int)icmpv6.GetType () >= 133) && ((int)icmpv6.GetType () <= 137)))
+        {
+          NS_TEST_EXPECT_MSG_EQ ((int) icmpv6.GetType (),
+                                 Icmpv6Header::ICMPV6_ERROR_TIME_EXCEEDED,
+                                 "The received Packet is not a ICMPV6_ERROR_TIME_EXCEEDED");
+        }
     }
 }
 
@@ -608,57 +608,57 @@ void
 IcmpV6TimeExceedTestCase::DoRun ()
 {
   NodeContainer n, n0n1,n1n2;
-  n.Create(3);
-  n0n1.Add(n.Get(0));
-  n0n1.Add(n.Get(1));
-  n1n2.Add(n.Get(1));
-  n1n2.Add(n.Get(2));
+  n.Create (3);
+  n0n1.Add (n.Get (0));
+  n0n1.Add (n.Get (1));
+  n1n2.Add (n.Get (1));
+  n1n2.Add (n.Get (2));
 
   Ptr<SimpleChannel> channel = CreateObject <SimpleChannel> ();
   Ptr<SimpleChannel> channel2 = CreateObject <SimpleChannel> ();
 
   SimpleNetDeviceHelper simpleHelper;
-  simpleHelper.SetNetDevicePointToPointMode(true);
+  simpleHelper.SetNetDevicePointToPointMode (true);
 
   SimpleNetDeviceHelper simpleHelper2;
-  simpleHelper2.SetNetDevicePointToPointMode(true);
+  simpleHelper2.SetNetDevicePointToPointMode (true);
 
   NetDeviceContainer devices;
-  devices = simpleHelper.Install(n0n1,channel);
+  devices = simpleHelper.Install (n0n1,channel);
 
   NetDeviceContainer devices2;
-  devices2 = simpleHelper2.Install(n1n2,channel2);
+  devices2 = simpleHelper2.Install (n1n2,channel2);
 
   InternetStackHelper internet;
   internet.Install (n);
 
   Ipv6AddressHelper address;
 
-  address.NewNetwork();
+  address.NewNetwork ();
   address.SetBase (Ipv6Address ("2001:1::"), Ipv6Prefix (64));
 
-  Ipv6InterfaceContainer interfaces = address.Assign(devices);
-  interfaces.SetForwarding(1,true);
-  interfaces.SetDefaultRouteInAllNodes(1);
+  Ipv6InterfaceContainer interfaces = address.Assign (devices);
+  interfaces.SetForwarding (1,true);
+  interfaces.SetDefaultRouteInAllNodes (1);
   address.SetBase (Ipv6Address ("2001:2::"), Ipv6Prefix (64));
-  Ipv6InterfaceContainer interfaces2 = address.Assign(devices2);
+  Ipv6InterfaceContainer interfaces2 = address.Assign (devices2);
 
-  interfaces2.SetForwarding(0,true);
-  interfaces2.SetDefaultRouteInAllNodes(0);
+  interfaces2.SetForwarding (0,true);
+  interfaces2.SetDefaultRouteInAllNodes (0);
 
   Ptr<Socket> socket;
   socket = Socket::CreateSocket (n.Get (0), TypeId::LookupByName ("ns3::Ipv6RawSocketFactory"));
   socket->SetAttribute ("Protocol", UintegerValue (Ipv6Header::IPV6_ICMPV6));
   socket->SetRecvCallback (MakeCallback (&IcmpV6TimeExceedTestCase::ReceivePkt, this));
 
-  Inet6SocketAddress src = Inet6SocketAddress (Ipv6Address::GetAny(), 0);
+  Inet6SocketAddress src = Inet6SocketAddress (Ipv6Address::GetAny (), 0);
   NS_TEST_EXPECT_MSG_EQ (socket->Bind (src),0," SocketV6 Binding failed");
 
   // In Ipv6 TTL is renamed hop limit in IPV6.
   // The hop limit is not big enough , causing an ICMPV6 Time Exceeded error
-  socket->SetIpv6HopLimit(1);
+  socket->SetIpv6HopLimit (1);
 
-  SendData (socket, interfaces2.GetAddress(1,1));
+  SendData (socket, interfaces2.GetAddress (1,1));
 
   NS_TEST_EXPECT_MSG_EQ (m_receivedPacket->GetSize (), 72, " Unexpected ICMPV6_ECHO_REPLY packet size");
 
