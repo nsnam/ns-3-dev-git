5fb0c876884e2cd60b02d84abd182518130ee591
Alexander Krotov <krotov@iitp.ru>
2019-09-30 11:55:40 +0300

    wifi: fix indentation in wifi-utils.h

--- src/wifi/model/wifi-utils.h
+++ src/wifi/model/wifi-utils.h
@@ -183,22 +183,22 @@ uint32_t GetSize (Ptr<const Packet> packet, const WifiMacHeader *hdr, bool isAmp
  */
 bool IsAmpdu (Ptr<const Packet> packet);
 
-  /**
-   * Get the maximum PPDU duration (see Section 10.14 of 802.11-2016) for
-   * the PHY layers defining the aPPDUMaxTime characteristic (HT, VHT and HE).
-   * Return zero otherwise.
-   *
-   * \param preamble the preamble type
-   *
-   * \return the maximum PPDU duration, if defined, and zero otherwise
-   */
-  Time GetPpduMaxTime (WifiPreamble preamble);
+/**
+ * Get the maximum PPDU duration (see Section 10.14 of 802.11-2016) for
+ * the PHY layers defining the aPPDUMaxTime characteristic (HT, VHT and HE).
+ * Return zero otherwise.
+ *
+ * \param preamble the preamble type
+ *
+ * \return the maximum PPDU duration, if defined, and zero otherwise
+ */
+Time GetPpduMaxTime (WifiPreamble preamble);
 
-  /// Size of the space of sequence numbers
-  const uint16_t SEQNO_SPACE_SIZE = 4096;
+/// Size of the space of sequence numbers
+const uint16_t SEQNO_SPACE_SIZE = 4096;
 
-  /// Size of the half the space of sequence numbers (used to determine old packets)
-  const uint16_t SEQNO_SPACE_HALF_SIZE = SEQNO_SPACE_SIZE / 2;
+/// Size of the half the space of sequence numbers (used to determine old packets)
+const uint16_t SEQNO_SPACE_HALF_SIZE = SEQNO_SPACE_SIZE / 2;
 } // namespace ns3
 
 #endif /* WIFI_UTILS_H */
