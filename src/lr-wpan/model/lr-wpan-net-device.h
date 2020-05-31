b5bffe4bd28b730bda6d318cf8741263d4a54a56
Tommaso Pecorella <tommaso.pecorella@unifi.it>
2020-05-31 15:59:44 +0200

    lrwpan: clarify the role of U/L bit in the interface identifier.

--- src/lr-wpan/model/lr-wpan-net-device.h
+++ src/lr-wpan/model/lr-wpan-net-device.h
@@ -191,8 +191,17 @@ private:
   void CompleteConfig (void);
 
   /**
-   * Builds a Mac48Address from the PanId and Short Address
+   * Builds a "pseudo 48-bit address" from the PanId and Short Address
    * The form is PanId : 0x0 : 0x0 : ShortAddress
+   *
+   * The address follows RFC 4944, section 6, and it is used to build an
+   * Interface ID.
+   *
+   * The Interface ID should have its U/L bit is set to zero, to indicate that
+   * this interface ID is not globally unique.
+   * However, the U/L bit flipping is performed when the IPv6 address is created.
+   *
+   * As a consequence, here we set it to 1.
    *
    * \param panId The PanID
    * \param shortMac The Short MAC address
