e315c878ed0964b0b8d0c2a17dcba73d28a4d659
Tom Henderson <tomh@tomh.org>
2020-07-03 13:25:02 -0700

    Add missing copyright

--- examples/tcp/tcp-pacing.cc
+++ examples/tcp/tcp-pacing.cc
@@ -1,5 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
+ * Copyright (c) 2017 NITK Surathkal
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
@@ -12,6 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Vivek Jain <jain.vivek.anand@gmail.com>
  */
 
 // Network topology
