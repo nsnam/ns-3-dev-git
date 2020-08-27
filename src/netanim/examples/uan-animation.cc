54e8615566d5a2c475a586671884f767fc54ad08
Jordan Dorham <dorham1@llnl.gov>
2020-08-27 10:51:06 -0700

    implementing time changes in netanim

--- src/netanim/examples/uan-animation.cc
+++ src/netanim/examples/uan-animation.cc
@@ -68,7 +68,7 @@ NetAnimExperiment::NetAnimExperiment ()
 void
 NetAnimExperiment::ResetData ()
 {
-  NS_LOG_DEBUG (Simulator::Now ().GetSeconds () << "  Resetting data");
+  NS_LOG_DEBUG (Simulator::Now ().As (Time::S) << "  Resetting data");
   m_throughputs.push_back (m_bytesTotal * 8.0 / m_simTime.GetSeconds ());
   m_bytesTotal = 0;
 }
@@ -96,7 +96,7 @@ void
 NetAnimExperiment::UpdatePositions (NodeContainer &nodes)
 {
 
-  NS_LOG_DEBUG (Simulator::Now ().GetSeconds () << " Updating positions");
+  NS_LOG_DEBUG (Simulator::Now ().As (Time::S) << " Updating positions");
   NodeContainer::Iterator it = nodes.Begin ();
   Ptr<UniformRandomVariable> uv = CreateObject<UniformRandomVariable> ();
   uv->SetAttribute ("Min", DoubleValue (0.0));
