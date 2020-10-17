d9f4d1e0171dd3129cf38e793f1475e60290d611
Tom Henderson <tomh@tomh.org>
2020-10-17 16:51:03 -0700

    dsr: Avoid possible segmentation fault in DsrRouting::DoDispose

--- src/dsr/model/dsr-routing.cc
+++ src/dsr/model/dsr-routing.cc
@@ -557,12 +557,16 @@ DsrRouting::DoDispose (void)
       Ptr<WifiNetDevice> wifi = dev->GetObject<WifiNetDevice> ();
       if (wifi != 0)
         {
-          Ptr<WifiMac> mac = wifi->GetMac ()->GetObject<AdhocWifiMac> ();
+          Ptr<WifiMac> mac = wifi->GetMac ();
           if (mac != 0)
             {
-              mac->TraceDisconnectWithoutContext ("TxErrHeader",
+              Ptr<AdhocWifiMac> adhoc = mac->GetObject<AdhocWifiMac> ();
+              if (adhoc)
+                {
+                  adhoc->TraceDisconnectWithoutContext ("TxErrHeader",
                                                   m_routeCache->GetTxErrorCallback ());
-              m_routeCache->DelArpCache (m_ipv4->GetInterface (i)->GetArpCache ());
+                  m_routeCache->DelArpCache (m_ipv4->GetInterface (i)->GetArpCache ());
+                }
             }
         }
     }
