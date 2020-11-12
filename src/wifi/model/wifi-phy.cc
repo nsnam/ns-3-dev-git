lanante/ns-3-dev-11ax

wifi: Make use of the new CalculateTxDuration function to compute PPDU duration

src/wifi/model/wifi-phy.cc
@@ -2410,6 +2410,7 @@ WifiPhy::CalculateTxDuration (uint32_t size, WifiTxVector txVector, uint16_t fre
 {
   Time duration = CalculatePlcpPreambleAndHeaderDuration (txVector)
     + GetPayloadDuration (size, txVector, frequency, NORMAL_MPDU, staId);
+  NS_ASSERT (duration.IsStrictlyPositive ());
   return duration;
 }
 
@@ -2601,8 +2602,7 @@ WifiPhy::Send (WifiPsduMap psdus, WifiTxVector txVector)
       return;
     }
 
-  Time txDuration = CalculateTxDuration (psdus.at (STA_ID_SU)->GetSize (), txVector, GetFrequency ()); //TODO: extend CalculateTxDuration for MU
-  NS_ASSERT (txDuration.IsStrictlyPositive ());
+  Time txDuration = CalculateTxDuration (psdus, txVector, GetFrequency ());
 
   if (m_endPreambleDetectionEvent.IsRunning ())
     {

nsnam/ns-3-dev-git

src/wifi/model/wifi-phy.cc
@@ -2545,6 +2545,7 @@ WifiPhy::CalculateTxDuration (uint32_t size, WifiTxVector txVector, WifiPhyBand 
 {
   Time duration = CalculatePhyPreambleAndHeaderDuration (txVector)
     + GetPayloadDuration (size, txVector, band, NORMAL_MPDU, staId);
+  NS_ASSERT (duration.IsStrictlyPositive ());
   return duration;
 }
 
@@ -2737,8 +2738,7 @@ WifiPhy::Send (WifiConstPsduMap psdus, WifiTxVector txVector)
       return;
     }
 
-  Time txDuration = CalculateTxDuration (psdus.at (SU_STA_ID)->GetSize (), txVector, GetPhyBand ()); //TODO: extend CalculateTxDuration for MU
-  NS_ASSERT (txDuration.IsStrictlyPositive ());
+  Time txDuration = CalculateTxDuration (psdus, txVector, GetPhyBand ());
 
   if ((m_currentEvent != 0) && (m_currentEvent->GetEndTime () > (Simulator::Now () + m_state->GetDelayUntilIdle ())))
     {