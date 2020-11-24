c12ef3987d118bf72309607938cf818a5c58704e
bbojovic <biljana.bojovic@cttc.es>
2020-11-24 12:45:11 +0100

    lte: (fixes !273) When removing LC remove also its BSR

--- src/lte/model/lte-ue-mac.cc
+++ src/lte/model/lte-ue-mac.cc
@@ -591,6 +591,7 @@ LteUeMac::DoRemoveLc (uint8_t lcId)
   NS_LOG_FUNCTION (this << " lcId" << lcId);
   NS_ASSERT_MSG (m_lcInfoMap.find (lcId) != m_lcInfoMap.end (), "could not find LCID " << lcId);
   m_lcInfoMap.erase (lcId);
+  m_ulBsrReceived.erase(lcId); //empty BSR buffer for this lcId
 }
 
 void
