lanante/ns-3-dev-11ax

wifi: Use length field from TXVECTOR to compute the TX duration of a HE TB PPDU

src/wifi/model/wifi-phy.cc
@@ -2731,7 +2731,16 @@ WifiPhy::Send (WifiPsduMap psdus, WifiTxVector txVector)
       return;
     }
   
-  Time txDuration = CalculateTxDuration (psdus, txVector, GetFrequency ());
+  Time txDuration;
+  if (txVector.GetPreambleType () == WIFI_PREAMBLE_HE_TB)
+    {
+      NS_ASSERT (txVector.GetLength () > 0);
+      txDuration = ConvertLSigLengthToHeTbPpduDuration (txVector.GetLength (), txVector, GetFrequency ());
+    }
+  else
+    {
+      txDuration = CalculateTxDuration (psdus, txVector, GetFrequency ());
+    }
 
   for (auto & endPreambleDetectionEvent : m_endPreambleDetectionEvents)
     {

nsnam/ns-3-dev-git

src/wifi/model/wifi-phy.cc
@@ -2630,6 +2630,11 @@ WifiPhy::CalculateTxDuration (uint32_t size, WifiTxVector txVector, WifiPhyBand 
 Time
 WifiPhy::CalculateTxDuration (WifiConstPsduMap psduMap, WifiTxVector txVector, WifiPhyBand band)
 {
+  if (txVector.GetPreambleType () == WIFI_PREAMBLE_HE_TB)
+    {
+      return ConvertLSigLengthToHeTbPpduDuration (txVector.GetLength (), txVector, band);
+    }
+
   Time maxDuration = Seconds (0);
   for (auto & staIdPsdu : psduMap)
     {
@@ -2815,8 +2820,17 @@ WifiPhy::Send (WifiConstPsduMap psdus, WifiTxVector txVector)
         }
       return;
     }
-
-  Time txDuration = CalculateTxDuration (psdus, txVector, GetPhyBand ());
+  
+  Time txDuration;
+  if (txVector.GetPreambleType () == WIFI_PREAMBLE_HE_TB)
+    {
+      NS_ASSERT (txVector.GetLength () > 0);
+      txDuration = ConvertLSigLengthToHeTbPpduDuration (txVector.GetLength (), txVector, GetPhyBand ());
+    }
+  else
+    {
+      txDuration = CalculateTxDuration (psdus, txVector, GetPhyBand ());
+    }
 
   if ((m_currentEvent != 0) && (m_currentEvent->GetEndTime () > (Simulator::Now () + m_state->GetDelayUntilIdle ())))
     {