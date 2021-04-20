acec4714bbda0d1c9f4f510f2411a9a4de05df90
Tom Henderson <tomh@tomh.org>
2021-04-20 09:40:57 -0700

    examples: (fix of commit e353bafa) Set PCAP data link type

--- examples/tutorial/third.py
+++ examples/tutorial/third.py
@@ -90,7 +90,7 @@ wifiStaNodes.Create(nWifi)
 wifiApNode = p2pNodes.Get(0)
 
 channel = ns.wifi.YansWifiChannelHelper.Default()
-phy = ns.wifi.YansWifiPhyHelper.Default()
+phy = ns.wifi.YansWifiPhyHelper()
 phy.SetChannel(channel.Create())
 
 wifi = ns.wifi.WifiHelper()
@@ -152,7 +152,7 @@ ns.internet.Ipv4GlobalRoutingHelper.PopulateRoutingTables()
 ns.core.Simulator.Stop(ns.core.Seconds(10.0))
 
 if tracing == "True":
-  phy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+	phy.SetPcapDataLinkType(phy.DLT_IEEE802_11_RADIO)
 	pointToPoint.EnablePcapAll ("third")
 	phy.EnablePcap ("third", apDevices.Get (0))
 	csma.EnablePcap ("third", csmaDevices.Get (0), True)
