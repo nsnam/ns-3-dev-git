997f6969acfbb14f6841631579073f59e7e6e5b0
Tom Henderson <tomh@tomh.org>
2021-09-14 21:31:05 -0700

    buildings: Instrument tests to run group mobility examples

--- src/buildings/test/examples-to-run.py
+++ src/buildings/test/examples-to-run.py
@@ -10,6 +10,8 @@
 # See test.py for more information.
 cpp_examples = [
     ("buildings-pathloss-profiler", "True", "True"),
+    ("outdoor-group-mobility-example --useHelper=0", "True", "True"),
+    ("outdoor-group-mobility-example --useHelper=1", "True", "True"),
 ]
 
 # A list of Python examples to run in order to ensure that they remain
