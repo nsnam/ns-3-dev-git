52fa7235b2b05e7ac4c14fb6ef47c50f748e7167
Philip Hönnecke <p.hoennecke@tu-braunschweig.de>
2021-09-24 18:29:32 +0200

    energy: (fixes #423) SimpleDeviceEnergyModel calculation of consumed energy

--- src/energy/model/simple-device-energy-model.cc
+++ src/energy/model/simple-device-energy-model.cc
@@ -104,7 +104,7 @@ SimpleDeviceEnergyModel::SetCurrentA (double current)
 
   double energyToDecrease = 0.0;
   double supplyVoltage = m_source->GetSupplyVoltage ();
-  energyToDecrease = duration.GetSeconds () * current * supplyVoltage;
+  energyToDecrease = duration.GetSeconds () * m_actualCurrentA * supplyVoltage;
 
   // update total energy consumption
   m_totalEnergyConsumption += energyToDecrease;
