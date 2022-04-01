7b24d1fafb1bbfe291882c7911f9a7840d0f8f4d
Pierre Wendling <pierre.wendling.4@gmail.com>
2022-03-31 20:37:48 -0400

    core-config: Fix header detection and conflict. - Fixed `sys/stat.h` detection. - Fixed `HAVE_PTHREAD_H` define to avoid warning with Python includes. - Added missing quotes for the semaphore include check.

--- build-support/core-config-template.h
+++ build-support/core-config-template.h
@@ -15,7 +15,7 @@
 #cmakedefine01 HAVE_STDLIB_H
 #cmakedefine01 HAVE_GETENV
 #cmakedefine01 HAVE_SIGNAL_H
-#cmakedefine   HAVE_PTHREAD_H
+#cmakedefine01 HAVE_PTHREAD_H
 #cmakedefine   HAVE_RT
 
 #endif //NS3_CORE_CONFIG_H
