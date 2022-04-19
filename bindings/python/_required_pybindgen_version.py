8378bc9c4b07f294edad586fbb0ddbbfad9b3abe
Tom Henderson <tomh@tomh.org>
2022-04-19 09:59:27 -0700

    bindings: Add file to track required pybindgen version

---
+++ bindings/python/_required_pybindgen_version.py
@@ -0,0 +1,9 @@
+# Store version information for required pybindgen version, used by
+# ns-3-allinone/download.py.
+# If specifying a released pybindgen version, specify the required pybindgen
+# version as, e.g. '0.21.0'
+# If specifying a commit on the development tree, specify it like this based
+# on 'git describe --tags' command.  Example, if the latest release was 0.21.0,
+# and 'git describe --tags' reports "0.21.0-6-g8e7c0a9", then write the
+# version string below as '0.21.0.post6+ng8e7c0a9'
+__required_pybindgen_version__ = '0.22.1'
