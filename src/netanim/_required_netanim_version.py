b6a03e74f5c293349d648a0cb65979c8e7890113
Tom Henderson <tomh@tomh.org>
2022-04-21 14:14:41 -0700

    netanim: Add file to track required netanim version

---
+++ src/netanim/_required_netanim_version.py
@@ -0,0 +1,9 @@
+# Store version information for required netanim version, used by
+# ns-3-allinone/download.py.
+# If specifying a released netanim version, specify the required netanim
+# version as, e.g. 'netanim-3.108'
+# If specifying a commit on the development tree, specify it like this based
+# on 'git describe --tags' command.  Example, if the latest release was 3.108,
+# and 'git describe --tags' reports "netanim-3.108-6-g8e7c0a9", then write the
+# version string below as 'netanim-3.108.post6+ng8e7c0a9'
+__required_netanim_version__ = 'netanim-3.108'
