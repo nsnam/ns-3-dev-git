1c12440603293de2478f9e3651c3fbdf96b8be61
Sebastien Deronne <sebastien.deronne@gmail.com>
2022-07-26 20:27:05 +0200

    wifi: Add EHT PHY Capabilities Information field to EHT capabilities

--- src/wifi/model/eht/eht-capabilities.cc
+++ src/wifi/model/eht/eht-capabilities.cc
@@ -55,8 +55,107 @@ EhtMacCapabilities::Deserialize(Buffer::Iterator start)
     return 2;
 }
 
+uint16_t
+EhtPhyCapabilities::GetSize() const
+{
+    return 9;
+}
+
+void
+EhtPhyCapabilities::Serialize(Buffer::Iterator& start) const
+{
+    uint64_t val1 =
+        (support320MhzIn6Ghz << 1) | (support242ToneRuInBwLargerThan20Mhz << 2) |
+        (ndpWith4TimesEhtLtfAnd32usGi << 3) | (partialBandwidthUlMuMimo << 4) |
+        (suBeamformer << 5) | (suBeamformee << 6) | (beamformeeSsBwNotLargerThan80Mhz << 7) |
+        (beamformeeSs160Mhz << 10) | (beamformeeSs320Mhz << 13) |
+        (nSoundingDimensionsBwNotLargerThan80Mhz << 16) | (nSoundingDimensions160Mhz << 19) |
+        (nSoundingDimensions320Mhz << 22) | (ng16SuFeedback << 25) | (ng16MuFeedback << 26) |
+        (codebooksizeSuFeedback << 27) | (codebooksizeMuFeedback << 28) |
+        (triggeredSuBeamformingFeedback << 29) | (triggeredMuBeamformingPartialBwFeedback << 30) |
+        (triggeredCqiFeedback << 31) | (static_cast<uint64_t>(partialBandwidthDlMuMimo) << 32) |
+        (static_cast<uint64_t>(psrBasedSpatialReuseSupport) << 33) |
+        (static_cast<uint64_t>(powerBoostFactorSupport) << 34) |
+        (static_cast<uint64_t>(muPpdu4xEhtLtfAnd800nsGi) << 35) |
+        (static_cast<uint64_t>(maxNc) << 36) |
+        (static_cast<uint64_t>(nonTriggeredCqiFeedback) << 40) |
+        (static_cast<uint64_t>(supportTx1024And4096QamForRuSmallerThan242Tones) << 41) |
+        (static_cast<uint64_t>(supportRx1024And4096QamForRuSmallerThan242Tones) << 42) |
+        (static_cast<uint64_t>(ppeThresholdsPresent) << 43) |
+        (static_cast<uint64_t>(commonNominalPacketPadding) << 44) |
+        (static_cast<uint64_t>(maxNumSupportedEhtLtfs) << 46) |
+        (static_cast<uint64_t>(supportMcs15) << 51) |
+        (static_cast<uint64_t>(supportEhtDupIn6GHz) << 55) |
+        (static_cast<uint64_t>(support20MhzOperatingStaReceivingNdpWithWiderBw) << 56) |
+        (static_cast<uint64_t>(nonOfdmaUlMuMimoBwNotLargerThan80Mhz) << 57) |
+        (static_cast<uint64_t>(nonOfdmaUlMuMimo160Mhz) << 58) |
+        (static_cast<uint64_t>(nonOfdmaUlMuMimo320Mhz) << 59) |
+        (static_cast<uint64_t>(muBeamformerBwNotLargerThan80Mhz) << 60) |
+        (static_cast<uint64_t>(muBeamformer160Mhz) << 61) |
+        (static_cast<uint64_t>(muBeamformer320Mhz) << 62) |
+        (static_cast<uint64_t>(tbSoundingFeedbackRateLimit) << 63);
+    uint8_t val2 = rx1024QamInWiderBwDlOfdmaSupport | (rx4096QamInWiderBwDlOfdmaSupport << 1);
+    start.WriteHtolsbU64(val1);
+    start.WriteU8(val2);
+}
+
+uint16_t
+EhtPhyCapabilities::Deserialize(Buffer::Iterator start)
+{
+    Buffer::Iterator i = start;
+    uint64_t val1 = i.ReadLsbtohU64();
+    uint8_t val2 = i.ReadU8();
+
+    support320MhzIn6Ghz = (val1 >> 1) & 0x0001;
+    support242ToneRuInBwLargerThan20Mhz = (val1 >> 2) & 0x0001;
+    ndpWith4TimesEhtLtfAnd32usGi = (val1 >> 3) & 0x0001;
+    partialBandwidthUlMuMimo = (val1 >> 4) & 0x0001;
+    suBeamformer = (val1 >> 5) & 0x0001;
+    suBeamformee = (val1 >> 6) & 0x0001;
+    beamformeeSsBwNotLargerThan80Mhz = (val1 >> 7) & 0x0007;
+    beamformeeSs160Mhz = (val1 >> 10) & 0x0007;
+    beamformeeSs320Mhz = (val1 >> 13) & 0x0007;
+    nSoundingDimensionsBwNotLargerThan80Mhz = (val1 >> 16) & 0x0007;
+    nSoundingDimensions160Mhz = (val1 >> 19) & 0x0007;
+    nSoundingDimensions320Mhz = (val1 >> 22) & 0x0007;
+    ng16SuFeedback = (val1 >> 25) & 0x0001;
+    ng16MuFeedback = (val1 >> 26) & 0x0001;
+    codebooksizeSuFeedback = (val1 >> 27) & 0x0001;
+    codebooksizeMuFeedback = (val1 >> 28) & 0x0001;
+    triggeredSuBeamformingFeedback = (val1 >> 29) & 0x0001;
+    triggeredMuBeamformingPartialBwFeedback = (val1 >> 30) & 0x0001;
+    triggeredCqiFeedback = (val1 >> 31) & 0x0001;
+    partialBandwidthDlMuMimo = (val1 >> 32) & 0x0001;
+    psrBasedSpatialReuseSupport = (val1 >> 33) & 0x0001;
+    powerBoostFactorSupport = (val1 >> 34) & 0x0001;
+    muPpdu4xEhtLtfAnd800nsGi = (val1 >> 35) & 0x0001;
+    maxNc = (val1 >> 36) & 0x000f;
+    nonTriggeredCqiFeedback = (val1 >> 40) & 0x0001;
+    supportTx1024And4096QamForRuSmallerThan242Tones = (val1 >> 41) & 0x0001;
+    supportRx1024And4096QamForRuSmallerThan242Tones = (val1 >> 42) & 0x0001;
+    ppeThresholdsPresent = (val1 >> 43) & 0x0001;
+    commonNominalPacketPadding = (val1 >> 44) & 0x0003;
+    maxNumSupportedEhtLtfs = (val1 >> 46) & 0x001f;
+    supportMcs15 = (val1 >> 51) & 0x000f;
+    supportEhtDupIn6GHz = (val1 >> 55) & 0x0001;
+    support20MhzOperatingStaReceivingNdpWithWiderBw = (val1 >> 56) & 0x0001;
+    nonOfdmaUlMuMimoBwNotLargerThan80Mhz = (val1 >> 57) & 0x0001;
+    nonOfdmaUlMuMimo160Mhz = (val1 >> 58) & 0x0001;
+    nonOfdmaUlMuMimo320Mhz = (val1 >> 59) & 0x0001;
+    muBeamformerBwNotLargerThan80Mhz = (val1 >> 60) & 0x0001;
+    muBeamformer160Mhz = (val1 >> 61) & 0x0001;
+    muBeamformer320Mhz = (val1 >> 62) & 0x0001;
+    tbSoundingFeedbackRateLimit = (val1 >> 63) & 0x0001;
+
+    rx1024QamInWiderBwDlOfdmaSupport = val2 & 0x01;
+    rx4096QamInWiderBwDlOfdmaSupport = (val2 >> 1) & 0x01;
+
+    return 9;
+}
+
 EhtCapabilities::EhtCapabilities()
-    : m_macCapabilities{}
+    : m_macCapabilities{},
+      m_phyCapabilities{}
 {
 }
 
@@ -76,8 +175,8 @@ uint16_t
 EhtCapabilities::GetInformationFieldSize() const
 {
     uint16_t size = 1 + // ElementIdExt
-                    m_macCapabilities.GetSize();
-    // FIXME: currently only EHT MAC Capabilities Information
+                    m_macCapabilities.GetSize() + m_phyCapabilities.GetSize();
+    // FIXME: currently only EHT MAC and PHY Capabilities Information
     return size;
 }
 
@@ -142,6 +241,7 @@ void
 EhtCapabilities::SerializeInformationField(Buffer::Iterator start) const
 {
     m_macCapabilities.Serialize(start);
+    m_phyCapabilities.Serialize(start);
 }
 
 uint16_t
@@ -149,7 +249,12 @@ EhtCapabilities::DeserializeInformationField(Buffer::Iterator start, uint16_t le
 {
     uint16_t count = 0;
     Buffer::Iterator i = start;
-    count += m_macCapabilities.Deserialize(i);
+
+    uint16_t nBytes = m_macCapabilities.Deserialize(i);
+    i.Next(nBytes);
+    count += nBytes;
+
+    count += m_phyCapabilities.Deserialize(i);
     return count;
 }
 
--- src/wifi/model/eht/eht-capabilities.h
+++ src/wifi/model/eht/eht-capabilities.h
@@ -63,6 +63,85 @@ struct EhtMacCapabilities
 };
 
 /**
+ * EHT PHY Capabilities Info subfield.
+ * See IEEE 802.11be D1.5 9.4.2.313.3 EHT PHY Capabilities Information subfield
+ */
+struct EhtPhyCapabilities
+{
+    uint8_t support320MhzIn6Ghz : 1;                 //!< Support For 320 MHz In 6 GHz
+    uint8_t support242ToneRuInBwLargerThan20Mhz : 1; //!< Support For 242-tone RU In BW Wider Than
+                                                     //!< 20 MHz
+    uint8_t ndpWith4TimesEhtLtfAnd32usGi : 1;        //!< NDP With 4x EHT-LTF And 3.2 μs GI
+    uint8_t partialBandwidthUlMuMimo : 1;            //!< Partial Bandwidth UL MU-MIMO
+    uint8_t suBeamformer : 1;                        //!< SU Beamformer
+    uint8_t suBeamformee : 1;                        //!< SU Beamformee
+    uint8_t beamformeeSsBwNotLargerThan80Mhz : 3;    //!< Beamformee SS (≤ 80 MHz)
+    uint8_t beamformeeSs160Mhz : 3;                  //!< Beamformee SS (= 160 MHz)
+    uint8_t beamformeeSs320Mhz : 3;                  //!< Beamformee SS (= 320 MHz)
+    uint8_t nSoundingDimensionsBwNotLargerThan80Mhz : 3; //!< Beamformee SS (≤ 80 MHz)
+    uint8_t nSoundingDimensions160Mhz : 3;               //!< Beamformee SS (= 160 MHz)
+    uint8_t nSoundingDimensions320Mhz : 3;               //!< Beamformee SS (= 320 MHz)
+    uint8_t ng16SuFeedback : 1;         //!< Support for subcarrier grouping of 16 for SU feedback
+    uint8_t ng16MuFeedback : 1;         //!< Support for subcarrier grouping of 16 for MU feedback
+    uint8_t codebooksizeSuFeedback : 1; //!< Support for a codebook size for SU feedback.
+    uint8_t codebooksizeMuFeedback : 1; //!< Support for a codebook size for MU feedback.
+    uint8_t triggeredSuBeamformingFeedback : 1;          //!< Triggered SU Beamforming Feedback
+    uint8_t triggeredMuBeamformingPartialBwFeedback : 1; //!< Triggered MU Beamforming Partial BW
+                                                         //!< Feedback
+    uint8_t triggeredCqiFeedback : 1;                    //!< Triggered CQI Feedback
+    uint8_t partialBandwidthDlMuMimo : 1;                //!< Partial Bandwidth DL MU-MIMO
+    uint8_t psrBasedSpatialReuseSupport : 1;             //!< EHT PSR-Based SR Support
+    uint8_t powerBoostFactorSupport : 1;                 //!< Power Boost Factor Support
+    uint8_t muPpdu4xEhtLtfAnd800nsGi : 1; //!< EHT MU PPDU With 4x EHT-LTF And 0.8 μs GI
+    uint8_t maxNc : 4;                    //!< Max Nc
+    uint8_t nonTriggeredCqiFeedback : 1;  //!< Non-Triggered CQI Feedback
+    uint8_t supportTx1024And4096QamForRuSmallerThan242Tones : 1; //!< Tx 1024-QAM And 4096-QAM <
+                                                                 //!< 242-tone RU Support
+    uint8_t supportRx1024And4096QamForRuSmallerThan242Tones : 1; //!< Rx 1024-QAM And 4096-QAM <
+                                                                 //!< 242-tone RU Support
+    uint8_t ppeThresholdsPresent : 1;                            //!< PPE Thresholds Present
+    uint8_t commonNominalPacketPadding : 2;                      //!< Common Nominal Packet Padding
+    uint8_t maxNumSupportedEhtLtfs : 5; //!< Maximum Number Of Supported EHT-LTFs
+    uint8_t supportMcs15 : 4;           //!< Support Of MCS 15
+    uint8_t supportEhtDupIn6GHz : 1;    //!< Support Of EHT DUP (MCS 14) In 6 GHz
+    uint8_t
+        support20MhzOperatingStaReceivingNdpWithWiderBw : 1; //!< Support For 20 MHz Operating STA
+                                                             //!< Receiving NDP With Wider Bandwidth
+    uint8_t nonOfdmaUlMuMimoBwNotLargerThan80Mhz : 1; //!< Non-OFDMA UL MU-MIMO (BW ≤ 80 MHz)
+    uint8_t nonOfdmaUlMuMimo160Mhz : 1;               //!< Non-OFDMA UL MU-MIMO (BW = 160 MHz)
+    uint8_t nonOfdmaUlMuMimo320Mhz : 1;               //!< Non-OFDMA UL MU-MIMO (BW = 320 MHz)
+    uint8_t muBeamformerBwNotLargerThan80Mhz : 1;     //!< MU Beamformer (BW ≤ 80 MHz)
+    uint8_t muBeamformer160Mhz : 1;                   //!< MU Beamformer (BW = 160 MHz)
+    uint8_t muBeamformer320Mhz : 1;                   //!< MU Beamformer (BW = 320 MHz)
+    uint8_t tbSoundingFeedbackRateLimit : 1;          //!< TB Sounding Feedback Rate Limit
+    uint8_t
+        rx1024QamInWiderBwDlOfdmaSupport : 1; //!< Rx 1024-QAM In Wider Bandwidth DL OFDMA Support
+    uint8_t
+        rx4096QamInWiderBwDlOfdmaSupport : 1; //!< Rx 4096-QAM In Wider Bandwidth DL OFDMA Support
+
+    /**
+     * Get the size of the serialized EHT PHY capabilities subfield
+     *
+     * \return the size of the serialized EHT PHY capabilities subfield
+     */
+    uint16_t GetSize() const;
+    /**
+     * Serialize the EHT PHY capabilities subfield
+     *
+     * \param start iterator pointing to where the EHT PHY capabilities subfield should be written
+     * to
+     */
+    void Serialize(Buffer::Iterator& start) const;
+    /**
+     * Deserialize the EHT PHY capabilities subfield
+     *
+     * \param start iterator pointing to where the EHT PHY capabilities subfield should be read from
+     * \return the number of bytes read
+     */
+    uint16_t Deserialize(Buffer::Iterator start);
+};
+
+/**
  * \ingroup wifi
  *
  * The IEEE 802.11be EHT Capabilities
@@ -104,6 +183,7 @@ class EhtCapabilities : public WifiInformationElement
     uint16_t GetMaxMpduLength() const;
 
     EhtMacCapabilities m_macCapabilities; //!< EHT MAC Capabilities Info subfield
+    EhtPhyCapabilities m_phyCapabilities; //!< EHT PHY Capabilities Info subfield
 
   private:
     uint16_t GetInformationFieldSize() const override;
