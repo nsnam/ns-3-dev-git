529d30b0daa69a6653658283eb4a12e6688cfb2b
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2022-10-08 21:15:21 -0300

    core: handle sigaction on Windows

--- src/core/model/fatal-impl.cc
+++ src/core/model/fatal-impl.cc
@@ -27,6 +27,39 @@
 #include <iostream>
 #include <list>
 
+#ifdef __WIN32__
+struct sigaction
+{
+    void (*sa_handler)(int);
+    int sa_flags;
+    int sa_mask;
+};
+
+int
+sigaction(int sig, struct sigaction* action, struct sigaction* old)
+{
+    if (sig == -1)
+    {
+        return 0;
+    }
+    if (old == nullptr)
+    {
+        if (signal(sig, SIG_DFL) == SIG_ERR)
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        if (signal(sig, action->sa_handler) == SIG_ERR)
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+#endif
+
 /**
  * \file
  * \ingroup fatalimpl
