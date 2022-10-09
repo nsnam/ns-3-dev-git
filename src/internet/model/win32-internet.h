a29d8ad5524bf65fd97a94e827a93bdd9a56bf0c
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2022-10-08 21:21:53 -0300

    internet: add win32-internet.h internet header wrapper
    Undefines colliding symbols defined in winsock2.h.

---
+++ src/internet/model/win32-internet.h
@@ -0,0 +1,20 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#ifndef WIN32_INTERNET_H
+#define WIN32_INTERNET_H
+
+/* Winsock2.h is a cursed header that
+ * causes multiple name collisions
+ *
+ * We use this private header to prevent them
+ */
+
+#include <winsock2.h>
+#undef GetObject
+#undef SetPort
+#undef SendMessage
+#undef CreateFile
+#undef Rectangle
+#undef interface
+
+#endif // WIN32_INTERNET_H
