d9ffa55a699efea61f16ae9330af66d508b073ef
Eduardo Almeida <enmsa@outlook.pt>
2022-10-28 16:33:46 +0000

    olsr: Fix formatting of commented code

--- src/olsr/model/olsr-routing-protocol.cc
+++ src/olsr/model/olsr-routing-protocol.cc
@@ -2359,31 +2359,31 @@ RoutingProtocol::PopulateMprSelectorSet(const olsr::MessageHeader& msg,
 /// \param p the packet which couldn't be delivered by the MAC layer.
 ///
 void
-OLSR::mac_failed (Ptr<Packet> p)
+OLSR::mac_failed(Ptr<Packet> p)
 {
-  double now              = Simulator::Now ();
-  struct hdr_ip* ih       = HDR_IP (p);
-  struct hdr_cmn* ch      = HDR_CMN (p);
+    double now = Simulator::Now ();
+    struct hdr_ip* ih = HDR_IP (p);
+    struct hdr_cmn* ch = HDR_CMN (p);
 
-  debug ("%f: Node %d MAC Layer detects a breakage on link to %d\n",
-         now,
-         OLSR::node_id (ra_addr ()),
-         OLSR::node_id (ch->next_hop ()));
+    debug("%f: Node %d MAC Layer detects a breakage on link to %d\n",
+          now,
+          OLSR::node_id (ra_addr ()),
+          OLSR::node_id (ch->next_hop ()));
 
-  if ((uint32_t)ih->daddr () == IP_BROADCAST)
+    if ((uint32_t)ih->daddr () == IP_BROADCAST)
     {
-      drop (p, DROP_RTR_MAC_CALLBACK);
-      return;
+        drop (p, DROP_RTR_MAC_CALLBACK);
+        return;
     }
 
-  OLSR_link_tuple* link_tuple = state_.find_link_tuple (ch->next_hop ());
-  if (link_tuple)
+    OLSR_link_tuple* link_tuple = state_.find_link_tuple(ch->next_hop());
+    if (link_tuple)
     {
-      link_tuple->lost_time () = now + OLSR_NEIGHB_HOLD_TIME;
-      link_tuple->time ()      = now + OLSR_NEIGHB_HOLD_TIME;
-      nb_loss (link_tuple);
+        link_tuple->lost_time() = now + OLSR_NEIGHB_HOLD_TIME;
+        link_tuple->time() = now + OLSR_NEIGHB_HOLD_TIME;
+        nb_loss(link_tuple);
     }
-  drop (p, DROP_RTR_MAC_CALLBACK);
+    drop(p, DROP_RTR_MAC_CALLBACK);
 }
 #endif
 
