a8154a084cc6d349792062a73f39aa25d6c3a0c2
Agrim Saharia <17agrim@gmail.com>
2023-01-11 13:21:44 +0530

    refined tcp p2p code

--- scratch/tcp_p2p.cc
+++ scratch/tcp_p2p.cc
@@ -17,7 +17,7 @@
 
        point to point
     n0 -------------- n1
-          tcp cubic 
+            tcp  
 */
 
 using namespace ns3;
@@ -41,7 +41,7 @@ TraceCwnd()
 int main()
 {
     bool enable_log = false;
-    std::string tcp_type = "TcpCubic";
+    std::string tcp_type = "TcpBbr";
 
     if (enable_log)
     {
@@ -51,16 +51,13 @@ int main()
 
     NodeContainer p2pnodes(2);
 
-    // TypeId tid = TypeId::LookupByName("ns3::" + tcp_type);
-    // Config::Set("/NodeList/0/$ns3::TcpL4Protocol/SocketType", TypeIdValue(tid));
-    // Config::Set("/NodeList/1/$ns3::TcpL4Protocol/SocketType", TypeIdValue(tid));
     Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::" + tcp_type));
-    Config::SetDefault("ns3::TcpSocketBase::Sack", BooleanValue(false));
+    // Config::SetDefault("ns3::TcpSocketBase::Sack", BooleanValue(false));
     // Config::SetDefault("ns3::FifoQueueDisc::MaxSize", QueueSizeValue(QueueSize("10p")));
 
     PointToPointHelper p2phelper;
     p2phelper.SetChannelAttribute("Delay", StringValue("10ms"));
-    p2phelper.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2phelper.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
     // p2phelper.SetQueue("ns3::DropTailQueue", "MaxSize", StringValue("10p"));
 
     NetDeviceContainer p2pdevices = p2phelper.Install(p2pnodes);
@@ -77,16 +74,16 @@ int main()
     Ipv4InterfaceContainer p2pinterfaces = ipaddr.Assign(p2pdevices);
 
     BulkSendHelper 
-        blkhelpercubic("ns3::TcpSocketFactory", InetSocketAddress(p2pinterfaces.GetAddress(1), 800));
+        blkhelper("ns3::TcpSocketFactory", InetSocketAddress(p2pinterfaces.GetAddress(1), 800));
 
     PacketSinkHelper sinkhelper("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 800));
 
     ApplicationContainer apps;
-    apps.Add(blkhelpercubic.Install(p2pnodes.Get(0)));
+    apps.Add(blkhelper.Install(p2pnodes.Get(0)));
     apps.Add(sinkhelper.Install(p2pnodes.Get(1)));
 
     apps.Get(0)->SetStartTime(Seconds(1.0));
-    apps.Get(0)->SetStopTime(Seconds(30.0));
+    apps.Get(0)->SetStopTime(Seconds(20.0));
 
     apps.Get(1)->SetStartTime(Seconds(0.0));
     apps.Get(1)->SetStopTime(Seconds(50.0));
@@ -95,7 +92,7 @@ int main()
 
     Simulator::Schedule(Seconds(1.001), TraceCwnd);
 
-    // AnimationInterface anim("../animbbr.xml");
+    // AnimationInterface anim("../anim.xml");
 
     p2phelper.EnablePcapAll("p2p");
 
