167c5ac75309b80a9cc638e3596a52e0d8490209
Rami Abdallah <abdallah.rami@gmail.com>
2024-02-27 23:23:53 +0100

    wifi: Fill in 6 GHz Operation Information field in HE Operation IE
    ...when included in a frame sent on a 6 GHz link

--- src/wifi/model/ap-wifi-mac.cc
+++ src/wifi/model/ap-wifi-mac.cc
@@ -1059,6 +1059,21 @@ ApWifiMac::GetHeOperation(uint8_t linkId) const
     }
     operation.m_bssColorInfo.m_bssColor = GetHeConfiguration()->GetBssColor();
 
+    if (auto phy = GetWifiPhy(linkId); phy && phy->GetPhyBand() == WIFI_PHY_BAND_6GHZ)
+    {
+        HeOperation::OpInfo6GHz op6Ghz;
+        const auto bw = phy->GetChannelWidth();
+        const auto ch = phy->GetOperatingChannel();
+        op6Ghz.m_chWid = (bw == 20) ? 0 : (bw == 40) ? 1 : (bw == 80) ? 2 : 3;
+        op6Ghz.m_primCh = ch.GetPrimaryChannelNumber(20, WIFI_STANDARD_80211ax);
+        op6Ghz.m_chCntrFreqSeg0 =
+            (bw == 160) ? ch.GetPrimaryChannelNumber(80, WIFI_STANDARD_80211ax) : ch.GetNumber();
+        // TODO: for 80+80 MHz channels, set this field to the secondary 80 MHz segment number
+        op6Ghz.m_chCntrFreqSeg1 = (bw == 160) ? ch.GetNumber() : 0;
+
+        operation.m_6GHzOpInfo = op6Ghz;
+    }
+
     return operation;
 }
 
