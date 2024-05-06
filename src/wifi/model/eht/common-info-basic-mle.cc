192c1e54b251a71e0ae8f7f773b1c1bbf06c903f
Stefano Avallone <stavallo@gmail.com>
2024-05-06 11:00:47 +0200

    wifi: Move Common Info Basic MLE implementation to a separate file

---
+++ src/wifi/model/eht/common-info-basic-mle.cc
@@ -0,0 +1,277 @@
+/*
+ * Copyright (c) 2021 Universita' degli Studi di Napoli Federico II
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Stefano Avallone <stavallo@unina.it>
+ */
+
+#include "common-info-basic-mle.h"
+
+#include "ns3/address-utils.h"
+
+namespace ns3
+{
+
+uint16_t
+CommonInfoBasicMle::GetPresenceBitmap() const
+{
+    // see Sec. 9.4.2.312.2.1 of 802.11be D1.5
+    return (m_linkIdInfo.has_value() ? 0x0001 : 0x0) |
+           (m_bssParamsChangeCount.has_value() ? 0x0002 : 0x0) |
+           (m_mediumSyncDelayInfo.has_value() ? 0x0004 : 0x0) |
+           (m_emlCapabilities.has_value() ? 0x0008 : 0x0) |
+           (m_mldCapabilities.has_value() ? 0x0010 : 0x0);
+}
+
+uint8_t
+CommonInfoBasicMle::GetSize() const
+{
+    uint8_t ret = 7; // Common Info Length (1) + MLD MAC Address (6)
+    ret += (m_linkIdInfo.has_value() ? 1 : 0);
+    ret += (m_bssParamsChangeCount.has_value() ? 1 : 0);
+    ret += (m_mediumSyncDelayInfo.has_value() ? 2 : 0);
+    ret += (m_emlCapabilities.has_value() ? 2 : 0);
+    ret += (m_mldCapabilities.has_value() ? 2 : 0);
+    return ret;
+}
+
+void
+CommonInfoBasicMle::Serialize(Buffer::Iterator& start) const
+{
+    start.WriteU8(GetSize()); // Common Info Length
+    WriteTo(start, m_mldMacAddress);
+    if (m_linkIdInfo.has_value())
+    {
+        start.WriteU8(*m_linkIdInfo & 0x0f);
+    }
+    if (m_bssParamsChangeCount.has_value())
+    {
+        start.WriteU8(*m_bssParamsChangeCount);
+    }
+    if (m_mediumSyncDelayInfo.has_value())
+    {
+        start.WriteU8(m_mediumSyncDelayInfo->mediumSyncDuration);
+        uint8_t val = m_mediumSyncDelayInfo->mediumSyncOfdmEdThreshold |
+                      (m_mediumSyncDelayInfo->mediumSyncMaxNTxops << 4);
+        start.WriteU8(val);
+    }
+    if (m_emlCapabilities.has_value())
+    {
+        uint16_t val =
+            m_emlCapabilities->emlsrSupport | (m_emlCapabilities->emlsrPaddingDelay << 1) |
+            (m_emlCapabilities->emlsrTransitionDelay << 4) |
+            (m_emlCapabilities->emlmrSupport << 7) | (m_emlCapabilities->emlmrDelay << 8) |
+            (m_emlCapabilities->transitionTimeout << 11);
+        start.WriteHtolsbU16(val);
+    }
+    if (m_mldCapabilities.has_value())
+    {
+        uint16_t val =
+            m_mldCapabilities->maxNSimultaneousLinks | (m_mldCapabilities->srsSupport << 4) |
+            (m_mldCapabilities->tidToLinkMappingSupport << 5) |
+            (m_mldCapabilities->freqSepForStrApMld << 7) | (m_mldCapabilities->aarSupport << 12);
+        start.WriteHtolsbU16(val);
+    }
+}
+
+uint8_t
+CommonInfoBasicMle::Deserialize(Buffer::Iterator start, uint16_t presence)
+{
+    Buffer::Iterator i = start;
+
+    uint8_t length = i.ReadU8();
+    ReadFrom(i, m_mldMacAddress);
+    uint8_t count = 7;
+
+    if ((presence & 0x0001) != 0)
+    {
+        m_linkIdInfo = i.ReadU8() & 0x0f;
+        count++;
+    }
+    if ((presence & 0x0002) != 0)
+    {
+        m_bssParamsChangeCount = i.ReadU8();
+        count++;
+    }
+    if ((presence & 0x0004) != 0)
+    {
+        m_mediumSyncDelayInfo = MediumSyncDelayInfo();
+        m_mediumSyncDelayInfo->mediumSyncDuration = i.ReadU8();
+        uint8_t val = i.ReadU8();
+        m_mediumSyncDelayInfo->mediumSyncOfdmEdThreshold = val & 0x0f;
+        m_mediumSyncDelayInfo->mediumSyncMaxNTxops = (val >> 4) & 0x0f;
+        count += 2;
+    }
+    if ((presence & 0x0008) != 0)
+    {
+        m_emlCapabilities = EmlCapabilities();
+        uint16_t val = i.ReadLsbtohU16();
+        m_emlCapabilities->emlsrSupport = val & 0x0001;
+        m_emlCapabilities->emlsrPaddingDelay = (val >> 1) & 0x0007;
+        m_emlCapabilities->emlsrTransitionDelay = (val >> 4) & 0x0007;
+        m_emlCapabilities->emlmrSupport = (val >> 7) & 0x0001;
+        m_emlCapabilities->emlmrDelay = (val >> 8) & 0x0007;
+        m_emlCapabilities->transitionTimeout = (val >> 11) & 0x000f;
+        count += 2;
+    }
+    if ((presence & 0x0010) != 0)
+    {
+        m_mldCapabilities = MldCapabilities();
+        uint16_t val = i.ReadLsbtohU16();
+        m_mldCapabilities->maxNSimultaneousLinks = val & 0x000f;
+        m_mldCapabilities->srsSupport = (val >> 4) & 0x0001;
+        m_mldCapabilities->tidToLinkMappingSupport = (val >> 5) & 0x0003;
+        m_mldCapabilities->freqSepForStrApMld = (val >> 7) & 0x001f;
+        m_mldCapabilities->aarSupport = (val >> 12) & 0x0001;
+        count += 2;
+    }
+
+    NS_ABORT_MSG_IF(count != length,
+                    "Common Info Length (" << +length
+                                           << ") differs "
+                                              "from actual number of bytes read ("
+                                           << +count << ")");
+    return count;
+}
+
+uint8_t
+CommonInfoBasicMle::EncodeEmlsrPaddingDelay(Time delay)
+{
+    auto delayUs = delay.GetMicroSeconds();
+
+    if (delayUs == 0)
+    {
+        return 0;
+    }
+
+    for (uint8_t i = 1; i <= 4; i++)
+    {
+        if (1 << (i + 4) == delayUs)
+        {
+            return i;
+        }
+    }
+
+    NS_ABORT_MSG("Value not allowed (" << delay.As(Time::US) << ")");
+    return 0;
+}
+
+Time
+CommonInfoBasicMle::DecodeEmlsrPaddingDelay(uint8_t value)
+{
+    NS_ABORT_MSG_IF(value > 4, "Value not allowed (" << +value << ")");
+    if (value == 0)
+    {
+        return MicroSeconds(0);
+    }
+    return MicroSeconds(1 << (4 + value));
+}
+
+uint8_t
+CommonInfoBasicMle::EncodeEmlsrTransitionDelay(Time delay)
+{
+    auto delayUs = delay.GetMicroSeconds();
+
+    if (delayUs == 0)
+    {
+        return 0;
+    }
+
+    for (uint8_t i = 1; i <= 5; i++)
+    {
+        if (1 << (i + 3) == delayUs)
+        {
+            return i;
+        }
+    }
+
+    NS_ABORT_MSG("Value not allowed (" << delay.As(Time::US) << ")");
+    return 0;
+}
+
+Time
+CommonInfoBasicMle::DecodeEmlsrTransitionDelay(uint8_t value)
+{
+    NS_ABORT_MSG_IF(value > 5, "Value not allowed (" << +value << ")");
+    if (value == 0)
+    {
+        return MicroSeconds(0);
+    }
+    return MicroSeconds(1 << (3 + value));
+}
+
+void
+CommonInfoBasicMle::SetMediumSyncDelayTimer(Time delay)
+{
+    int64_t delayUs = delay.GetMicroSeconds();
+    NS_ABORT_MSG_IF(delayUs % 32 != 0, "Delay must be a multiple of 32 microseconds");
+    delayUs /= 32;
+
+    if (!m_mediumSyncDelayInfo.has_value())
+    {
+        m_mediumSyncDelayInfo = CommonInfoBasicMle::MediumSyncDelayInfo{};
+    }
+    m_mediumSyncDelayInfo->mediumSyncDuration = (delayUs & 0xff);
+}
+
+Time
+CommonInfoBasicMle::GetMediumSyncDelayTimer() const
+{
+    NS_ASSERT(m_mediumSyncDelayInfo);
+    return MicroSeconds(m_mediumSyncDelayInfo->mediumSyncDuration * 32);
+}
+
+void
+CommonInfoBasicMle::SetMediumSyncOfdmEdThreshold(int8_t threshold)
+{
+    NS_ABORT_MSG_IF(threshold < -72 || threshold > -62, "Threshold may range from -72 to -62 dBm");
+    uint8_t value = 72 + threshold;
+
+    if (!m_mediumSyncDelayInfo.has_value())
+    {
+        m_mediumSyncDelayInfo = CommonInfoBasicMle::MediumSyncDelayInfo{};
+    }
+    m_mediumSyncDelayInfo->mediumSyncOfdmEdThreshold = value;
+}
+
+int8_t
+CommonInfoBasicMle::GetMediumSyncOfdmEdThreshold() const
+{
+    NS_ASSERT(m_mediumSyncDelayInfo);
+    return (m_mediumSyncDelayInfo->mediumSyncOfdmEdThreshold) - 72;
+}
+
+void
+CommonInfoBasicMle::SetMediumSyncMaxNTxops(uint8_t nTxops)
+{
+    NS_ASSERT_MSG(nTxops < 16, "Value " << +nTxops << "cannot be encoded in 4 bits");
+
+    if (!m_mediumSyncDelayInfo.has_value())
+    {
+        m_mediumSyncDelayInfo = CommonInfoBasicMle::MediumSyncDelayInfo{};
+    }
+
+    if (nTxops == 0)
+    {
+        // no limit on max number of TXOPs
+        m_mediumSyncDelayInfo->mediumSyncMaxNTxops = 15;
+        return;
+    }
+
+    m_mediumSyncDelayInfo->mediumSyncMaxNTxops = --nTxops;
+}
+
+std::optional<uint8_t>
+CommonInfoBasicMle::GetMediumSyncMaxNTxops() const
+{
+    NS_ASSERT(m_mediumSyncDelayInfo);
+    uint8_t nTxops = m_mediumSyncDelayInfo->mediumSyncMaxNTxops;
+    if (nTxops == 15)
+    {
+        return std::nullopt;
+    }
+    return nTxops + 1;
+}
+
+} // namespace ns3
---
+++ src/wifi/model/eht/common-info-basic-mle.h
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2021 Universita' degli Studi di Napoli Federico II
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Stefano Avallone <stavallo@unina.it>
+ */
+
+#ifndef COMMON_INFO_BASIC_MLE_H
+#define COMMON_INFO_BASIC_MLE_H
+
+#include "ns3/buffer.h"
+#include "ns3/mac48-address.h"
+#include "ns3/nstime.h"
+
+#include <cstdint>
+#include <optional>
+
+namespace ns3
+{
+
+/**
+ * Common Info field of the Basic Multi-Link element.
+ * See Sec. 9.4.2.312.2.2 of 802.11be D1.5
+ */
+struct CommonInfoBasicMle
+{
+    /**
+     * Medium Synchronization Delay Information subfield
+     */
+    struct MediumSyncDelayInfo
+    {
+        uint8_t mediumSyncDuration;            //!< Medium Synchronization Duration
+        uint8_t mediumSyncOfdmEdThreshold : 4; //!< Medium Synchronization OFDM ED Threshold
+        uint8_t mediumSyncMaxNTxops : 4;       //!< Medium Synchronization MAximum Number of TXOPs
+    };
+
+    /**
+     * EML Capabilities subfield
+     */
+    struct EmlCapabilities
+    {
+        uint8_t emlsrSupport : 1;         //!< EMLSR Support
+        uint8_t emlsrPaddingDelay : 3;    //!< EMLSR Padding Delay
+        uint8_t emlsrTransitionDelay : 3; //!< EMLSR Transition Delay
+        uint8_t emlmrSupport : 1;         //!< EMLMR Support
+        uint8_t emlmrDelay : 3;           //!< EMLMR Delay
+        uint8_t transitionTimeout : 4;    //!< Transition Timeout
+    };
+
+    /**
+     * MLD Capabilities subfield
+     */
+    struct MldCapabilities
+    {
+        uint8_t maxNSimultaneousLinks : 4;   //!< Max number of simultaneous links
+        uint8_t srsSupport : 1;              //!< SRS Support
+        uint8_t tidToLinkMappingSupport : 2; //!< TID-To-Link Mapping Negotiation Supported
+        uint8_t freqSepForStrApMld : 5; //!< Frequency Separation For STR/AP MLD Type Indication
+        uint8_t aarSupport : 1;         //!< AAR Support
+    };
+
+    /**
+     * Subfields
+     */
+    Mac48Address m_mldMacAddress;                  //!< MLD MAC Address
+    std::optional<uint8_t> m_linkIdInfo;           //!< Link ID Info
+    std::optional<uint8_t> m_bssParamsChangeCount; //!< BSS Parameters Change Count
+    std::optional<MediumSyncDelayInfo>
+        m_mediumSyncDelayInfo;                        //!< Medium Synchronization Delay Information
+    std::optional<EmlCapabilities> m_emlCapabilities; //!< EML Capabilities
+    std::optional<MldCapabilities> m_mldCapabilities; //!< MLD Capabilities
+
+    /**
+     * Get the Presence Bitmap subfield of the Common Info field
+     *
+     * @return the Presence Bitmap subfield of the Common Info field
+     */
+    uint16_t GetPresenceBitmap() const;
+
+    /**
+     * Get the size of the serialized Common Info field
+     *
+     * @return the size of the serialized Common Info field
+     */
+    uint8_t GetSize() const;
+
+    /**
+     * Serialize the Common Info field
+     *
+     * @param start iterator pointing to where the Common Info field should be written to
+     */
+    void Serialize(Buffer::Iterator& start) const;
+
+    /**
+     * Deserialize the Common Info field
+     *
+     * @param start iterator pointing to where the Common Info field should be read from
+     * @param presence the value of the Presence Bitmap field indicating which subfields
+     *                 are present in the Common Info field
+     * @return the number of bytes read
+     */
+    uint8_t Deserialize(Buffer::Iterator start, uint16_t presence);
+
+    /**
+     * @param delay the EMLSR Padding delay
+     * @return the encoded value for the EMLSR Padding Delay subfield
+     */
+    static uint8_t EncodeEmlsrPaddingDelay(Time delay);
+
+    /**
+     * @param value the value for the EMLSR Padding Delay subfield
+     * @return the corresponding EMLSR Padding delay
+     */
+    static Time DecodeEmlsrPaddingDelay(uint8_t value);
+
+    /**
+     * @param delay the EMLSR Transition delay
+     * @return the encoded value for the EMLSR Transition Delay subfield
+     */
+    static uint8_t EncodeEmlsrTransitionDelay(Time delay);
+
+    /**
+     * @param value the value for the EMLSR Transition Delay subfield
+     * @return the corresponding EMLSR Transition delay
+     */
+    static Time DecodeEmlsrTransitionDelay(uint8_t value);
+
+    /**
+     * Set the Medium Synchronization Duration subfield of the Medium Synchronization
+     * Delay Information in the Common Info field.
+     *
+     * @param delay the timer duration (must be a multiple of 32 microseconds)
+     */
+    void SetMediumSyncDelayTimer(Time delay);
+
+    /**
+     * Set the Medium Synchronization OFDM ED Threshold subfield of the Medium Synchronization
+     * Delay Information in the Common Info field.
+     *
+     * @param threshold the threshold in dBm (ranges from -72 to -62 dBm)
+     */
+    void SetMediumSyncOfdmEdThreshold(int8_t threshold);
+
+    /**
+     * Set the Medium Synchronization Maximum Number of TXOPs subfield of the Medium Synchronization
+     * Delay Information in the Common Info field. A value of zero indicates no limit on the
+     * maximum number of TXOPs.
+     *
+     * @param nTxops the maximum number of TXOPs a non-AP STA is allowed to attempt to
+     *               initiate while the MediumSyncDelay timer is running at a non-AP STA
+     */
+    void SetMediumSyncMaxNTxops(uint8_t nTxops);
+
+    /**
+     * Get the Medium Synchronization Duration subfield of the Medium Synchronization Delay
+     * Information in the Common Info field. Make sure that the Medium Synchronization Delay
+     * Information subfield is present.
+     *
+     * @return the timer duration
+     */
+    Time GetMediumSyncDelayTimer() const;
+
+    /**
+     * Get the Medium Synchronization OFDM ED Threshold in dBm. Make sure that the Medium
+     * Synchronization Delay Information subfield is present.
+     *
+     * @return the threshold in dBm
+     */
+    int8_t GetMediumSyncOfdmEdThreshold() const;
+
+    /**
+     * Get the maximum number of TXOPs a non-AP STA is allowed to attempt to initiate
+     * while the MediumSyncDelay timer is running at a non-AP STA. If no value is returned,
+     * no limit is imposed on the number of TXOPs. Make sure that the Medium Synchronization
+     * Delay Information subfield is present.
+     *
+     * @return the number of TXOPs
+     */
+    std::optional<uint8_t> GetMediumSyncMaxNTxops() const;
+};
+
+} // namespace ns3
+
+#endif /* COMMON_INFO_BASIC_MLE_H */
