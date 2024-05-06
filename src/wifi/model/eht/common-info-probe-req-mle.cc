7c54cf1212ba42724d86a82c2faffeb7471c8483
Sharan Naribole <sharan.naribole@gmail.com>
2024-05-06 11:37:33 +0200

    wifi: Add Probe Request Multi-link Element Common Info

---
+++ src/wifi/model/eht/common-info-probe-req-mle.cc
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Sharan Naribole <sharan.naribole@gmail.com>
+ */
+
+#include "common-info-probe-req-mle.h"
+
+#include <ns3/abort.h>
+
+namespace ns3
+{
+
+uint16_t
+CommonInfoProbeReqMle::GetPresenceBitmap() const
+{
+    // IEEE 802.11be D5.0 Figure 9-1072q
+    return (m_apMldId.has_value() ? 0x0001 : 0x0);
+}
+
+uint8_t
+CommonInfoProbeReqMle::GetSize() const
+{
+    uint8_t size = 1; // Common Info Length
+    size += (m_apMldId.has_value() ? 1 : 0);
+    return size;
+}
+
+void
+CommonInfoProbeReqMle::Serialize(Buffer::Iterator& start) const
+{
+    start.WriteU8(GetSize()); // Common Info Length
+    if (m_apMldId.has_value())
+    {
+        start.WriteU8(*m_apMldId);
+    }
+}
+
+uint8_t
+CommonInfoProbeReqMle::Deserialize(Buffer::Iterator start, uint16_t presence)
+{
+    auto i = start;
+    auto length = i.ReadU8();
+    uint8_t count = 1;
+
+    if (presence & 0x1)
+    {
+        m_apMldId = i.ReadU8();
+        count++;
+    }
+    NS_ABORT_MSG_IF(count != length,
+                    "Common Info Length (" << +length
+                                           << ") differs "
+                                              "from actual number of bytes read ("
+                                           << +count << ")");
+    return count;
+}
+
+} // namespace ns3
---
+++ src/wifi/model/eht/common-info-probe-req-mle.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2024
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Sharan Naribole <sharan.naribole@gmail.com>
+ */
+
+#ifndef COMMON_INFO_PROBE_REQ_MLE_H
+#define COMMON_INFO_PROBE_REQ_MLE_H
+
+#include <ns3/buffer.h>
+
+#include <cstdint>
+#include <optional>
+
+namespace ns3
+{
+
+/**
+ * Common Info field of Multi-link Element Probe Request variant.
+ * IEEE 802.11be D6.0 9.4.2.321.3
+ */
+struct CommonInfoProbeReqMle
+{
+    std::optional<uint8_t> m_apMldId; ///< AP MLD ID
+
+    /**
+     * Get the Presence Bitmap subfield of the Common Info field
+     *
+     * @return the Presence Bitmap subfield of the Common Info field
+     */
+    uint16_t GetPresenceBitmap() const;
+
+    /**
+     * Get the size of the serialized Common Info field
+     *
+     * @return the size of the serialized Common Info field
+     */
+    uint8_t GetSize() const;
+
+    /**
+     * Serialize the Common Info field
+     *
+     * @param start iterator pointing to where the Common Info field should be written to
+     */
+    void Serialize(Buffer::Iterator& start) const;
+
+    /**
+     * Deserialize the Common Info field
+     *
+     * @param start iterator pointing to where the Common Info field should be read from
+     * @param presence the value of the Presence Bitmap field indicating which subfields
+     *                 are present in the Common Info field
+     * @return the number of bytes read
+     */
+    uint8_t Deserialize(Buffer::Iterator start, uint16_t presence);
+};
+
+} // namespace ns3
+
+#endif // COMMON_INFO_PROBE_REQ_MLE_H
