3d29752bed3c4f3a5451824432ed7a4527113365
Sébastien Deronne <sebastien.deronne@gmail.com>
2024-06-03 21:31:13 +0200

    wifi: Enable logs for WifiMacQueueScheduler in WifiHelper::EnableLogComponents

--- src/wifi/helper/wifi-helper.cc
+++ src/wifi/helper/wifi-helper.cc
@@ -1288,6 +1288,7 @@ WifiHelper::EnableLogComponents(LogLevel logLevel)
     LogComponentEnable("WifiDefaultProtectionManager", logLevel);
     LogComponentEnable("WifiMac", logLevel);
     LogComponentEnable("WifiMacQueue", logLevel);
+    LogComponentEnable("WifiMacQueueScheduler", logLevel);
     LogComponentEnable("WifiMpdu", logLevel);
     LogComponentEnable("WifiNetDevice", logLevel);
     LogComponentEnable("WifiPhyStateHelper", logLevel);
