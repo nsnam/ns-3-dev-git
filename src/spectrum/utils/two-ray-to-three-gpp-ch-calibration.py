235588143c313fe8c2b23053ac56d7e588a22cc3
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2024-06-20 10:39:55 +0200

    spectrum: Switch license of two-ray-to-three-gpp-ch-calibration.py to GPLv2
    Matteo Pagin accepted the switch, see [1].
    
    [1] https://gitlab.com/nsnam/ns-3-dev/-/merge_requests/2030#note_1959169561

--- src/spectrum/utils/two-ray-to-three-gpp-ch-calibration.py
+++ src/spectrum/utils/two-ray-to-three-gpp-ch-calibration.py
@@ -6,10 +6,9 @@ Companion TwoRaySpectrumPropagationLossModel calibration script
 Copyright (c) 2022 SIGNET Lab, Department of Information Engineering,
 University of Padova
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 2 as published by the Free
+Software Foundation;
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
