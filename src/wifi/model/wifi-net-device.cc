3eeaa8c7608ec1199865f016f2e9f2360ff75aa9
Sébastien Deronne <sebastien.deronne@gmail.com>
2024-06-29 10:34:27 +0200

    wifi: Add logging to indicate link is UP or DOWN

--- src/wifi/model/wifi-net-device.cc
+++ src/wifi/model/wifi-net-device.cc
@@ -554,14 +554,18 @@ WifiNetDevice::ForwardUp(Ptr<const Packet> packet, Mac48Address from, Mac48Addre
 void
 WifiNetDevice::LinkUp()
 {
+    NS_LOG_FUNCTION(this);
     m_linkUp = true;
+    NS_LOG_INFO("Link is UP");
     m_linkChanges();
 }
 
 void
 WifiNetDevice::LinkDown()
 {
+    NS_LOG_FUNCTION(this);
     m_linkUp = false;
+    NS_LOG_INFO("Link is DOWN");
     m_linkChanges();
 }
 
