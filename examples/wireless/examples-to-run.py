5d758d1cfee73d64f66b3363734ea1090931a5e1
Sébastien Deronne <sebastien.deronne@gmail.com>
2024-08-12 12:22:10 +0200

    examples: Add wifi P2P with use of TID-to-link mapping to examples to run

--- examples/wireless/examples-to-run.py
+++ examples/wireless/examples-to-run.py
@@ -359,6 +359,11 @@ cpp_examples = [
         "True",
         "True",
     ),
+    (
+        "wifi-p2p --simulationTime=1s --numLinksAp=3 --numLinksSta=3 --numLinksP2p=1 --linksOverlap=0 --p2pLinkId=2 --adhocToStaMinExpectedThroughput=49 --staToAdhocMinExpectedThroughput=49 --staToAdhocMaxExpectedLoss=1.0 --adhocToStaMaxExpectedLoss=1.0",
+        "True",
+        "True",
+    ),
 ]
 
 # A list of Python examples to run in order to ensure that they remain
