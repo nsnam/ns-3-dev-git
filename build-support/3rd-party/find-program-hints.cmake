4bf311da47f2193732820b8c32ad4b65d3b8ebed
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2024-10-10 10:10:41 +0200

    build: Add SPDX license

--- build-support/3rd-party/find-program-hints.cmake
+++ build-support/3rd-party/find-program-hints.cmake
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 Universidade de Brasília
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Author: Gabriel Ferreira <gabrielcarvfer@gmail.com>
+
 disable_cmake_warnings()
 string(REPLACE ";" "/bin;" syspaths "${CMAKE_SYSTEM_PREFIX_PATH};")
 set(3RD_PARTY_FIND_PROGRAM_HINTS
