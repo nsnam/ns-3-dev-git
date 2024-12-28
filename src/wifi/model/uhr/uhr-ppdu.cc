e00caa1e94e02acaafe8614b9b52e1666ca2f8a2
Sébastien Deronne <sebastien.deronne@gmail.com>
2024-12-28 16:58:11 +0100

    wifi: Add minimal support for UHR PPDUs

---
+++ src/wifi/model/uhr/uhr-ppdu.cc
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2024 DERONNE SOFTWARE ENGINEERING
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Sébastien Deronne <sebastien.deronne@gmail.com>
+ */
+
+#include "uhr-ppdu.h"
+
+#include "ns3/log.h"
+#include "ns3/wifi-phy-operating-channel.h"
+#include "ns3/wifi-psdu.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("UhrPpdu");
+
+UhrPpdu::UhrPpdu(const WifiConstPsduMap& psdus,
+                 const WifiTxVector& txVector,
+                 const WifiPhyOperatingChannel& channel,
+                 Time ppduDuration,
+                 uint64_t uid,
+                 TxPsdFlag flag)
+    : EhtPpdu(psdus, txVector, channel, ppduDuration, uid, flag)
+{
+    NS_LOG_FUNCTION(this << psdus << txVector << channel << ppduDuration << uid << flag);
+}
+
+WifiPpduType
+UhrPpdu::GetType() const
+{
+    if (m_psdus.contains(SU_STA_ID))
+    {
+        return WIFI_PPDU_TYPE_SU;
+    }
+    switch (m_preamble)
+    {
+    case WIFI_PREAMBLE_UHR_MU:
+        return WIFI_PPDU_TYPE_DL_MU;
+    case WIFI_PREAMBLE_UHR_TB:
+        return WIFI_PPDU_TYPE_UL_MU;
+    case WIFI_PREAMBLE_UHR_ELR:
+        return WIFI_PPDU_TYPE_SU;
+    default:
+        NS_ASSERT_MSG(false, "invalid preamble " << m_preamble);
+        return WIFI_PPDU_TYPE_SU;
+    }
+}
+
+bool
+UhrPpdu::IsDlMu() const
+{
+    return (m_preamble == WIFI_PREAMBLE_UHR_MU) && !m_psdus.contains(SU_STA_ID);
+}
+
+bool
+UhrPpdu::IsUlMu() const
+{
+    return (m_preamble == WIFI_PREAMBLE_UHR_TB) && !m_psdus.contains(SU_STA_ID);
+}
+
+Ptr<WifiPpdu>
+UhrPpdu::Copy() const
+{
+    return Ptr<WifiPpdu>(new UhrPpdu(*this), false);
+}
+
+} // namespace ns3
---
+++ src/wifi/model/uhr/uhr-ppdu.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2024 DERONNE SOFTWARE ENGINEERING
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Sébastien Deronne <sebastien.deronne@gmail.com>
+ */
+
+#ifndef UHR_PPDU_H
+#define UHR_PPDU_H
+
+#include "ns3/eht-ppdu.h"
+
+/**
+ * @file
+ * @ingroup wifi
+ * Declaration of ns3::UhrPpdu class.
+ */
+
+namespace ns3
+{
+
+/**
+ * @brief UHR PPDU (11bn)
+ * @ingroup wifi
+ *
+ * TODO: UhrPpdu is currently identical to EhtPpdu
+ */
+class UhrPpdu : public EhtPpdu
+{
+  public:
+    /**
+     * Create an UHR PPDU, storing a map of PSDUs.
+     *
+     * @param psdus the PHY payloads (PSDUs)
+     * @param txVector the TXVECTOR that was used for this PPDU
+     * @param channel the operating channel of the PHY used to transmit this PPDU
+     * @param ppduDuration the transmission duration of this PPDU
+     * @param uid the unique ID of this PPDU or of the triggering PPDU if this is an EHT TB PPDU
+     * @param flag the flag indicating the type of Tx PSD to build
+     */
+    UhrPpdu(const WifiConstPsduMap& psdus,
+            const WifiTxVector& txVector,
+            const WifiPhyOperatingChannel& channel,
+            Time ppduDuration,
+            uint64_t uid,
+            TxPsdFlag flag);
+
+    WifiPpduType GetType() const override;
+    Ptr<WifiPpdu> Copy() const override;
+
+  private:
+    bool IsDlMu() const override;
+    bool IsUlMu() const override;
+}; // class UhrPpdu
+
+} // namespace ns3
+
+#endif /* UHR_PPDU_H */
