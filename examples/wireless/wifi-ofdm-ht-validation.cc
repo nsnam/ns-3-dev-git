187be11b1720dcd21725374ff7ee593850a6fca6
Sébastien Deronne <sebastien.deronne@gmail.com>
2025-01-03 16:22:27 +0100

    examples: Cleanup deprecated gnuplot command from wifi OFDM validation examples

--- examples/wireless/wifi-ofdm-ht-validation.cc
+++ examples/wireless/wifi-ofdm-ht-validation.cc
@@ -110,8 +110,7 @@ set style line 4 linewidth 5\n\
 set style line 5 linewidth 5\n\
 set style line 6 linewidth 5\n\
 set style line 7 linewidth 5\n\
-set style line 8 linewidth 5\n\
-set style increment user");
+set style line 8 linewidth 5\n");
     yansplot.GenerateOutput(yansfile);
     yansfile.close();
 
@@ -126,8 +125,7 @@ set style line 4 linewidth 5\n\
 set style line 5 linewidth 5\n\
 set style line 6 linewidth 5\n\
 set style line 7 linewidth 5\n\
-set style line 8 linewidth 5\n\
-set style increment user");
+set style line 8 linewidth 5\n");
 
     nistplot.GenerateOutput(nistfile);
     nistfile.close();
@@ -143,8 +141,7 @@ set style line 4 linewidth 5\n\
 set style line 5 linewidth 5\n\
 set style line 6 linewidth 5\n\
 set style line 7 linewidth 5\n\
-set style line 8 linewidth 5\n\
-set style increment user");
+set style line 8 linewidth 5\n");
 
     tableplot.GenerateOutput(tablefile);
     tablefile.close();
