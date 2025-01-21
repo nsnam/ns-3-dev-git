d24b08391e5a4baed818dae1a27f8b18e2e5b256
Eduardo Almeida <enmsa@outlook.pt>
2025-01-21 17:18:46 +0000

    build: Add support for clang-tidy-19 in CMake

--- build-support/macros-and-definitions.cmake
+++ build-support/macros-and-definitions.cmake
@@ -260,7 +260,7 @@ macro(process_options)
   if(${NS3_CLANG_TIDY})
     find_program(
       CLANG_TIDY NAMES clang-tidy clang-tidy-15 clang-tidy-16 clang-tidy-17
-                       clang-tidy-18
+                       clang-tidy-18 clang-tidy-19
     )
     if("${CLANG_TIDY}" STREQUAL "CLANG_TIDY-NOTFOUND")
       message(FATAL_ERROR "Clang-tidy was not found")
