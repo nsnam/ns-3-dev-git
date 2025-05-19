9947faee9f3ae13a583d5e70fbeebc7871751589
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2025-05-19 11:07:25 +0200

    doc: Move common variables to shared location

---
+++ doc/sphinx/defines.mk
@@ -0,0 +1,14 @@
+CONVERT = convert -density 300
+DIA = dia
+DOT = dot
+EPSTOPDF = epstopdf
+
+# You can set these variables from the command line.
+SPHINXOPTS    = -W --keep-going
+SPHINXBUILD   = sphinx-build
+PAPER         =
+BUILDDIR      = build
+
+# Internal variables.
+PAPEROPT_a4     = -D latex_paper_size=a4
+PAPEROPT_letter = -D latex_paper_size=letter
