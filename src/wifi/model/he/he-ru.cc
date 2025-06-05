31563292e590e0ce7973a2e0d3bfbaa4bb6e7d88
Sébastien Deronne <sebastien.deronne@gmail.com>
2025-06-05 19:37:00 +0200

    wifi: Use three-way comparison operators for HeRu::RuSpec and EhtRu::RuSpec

--- src/wifi/model/he/he-ru.cc
+++ src/wifi/model/he/he-ru.cc
@@ -790,30 +790,4 @@ HeRu::GetNumCentral26TonesRus(MHz_u bandwidth, RuType ruType)
     return nCentral26TonesRus;
 }
 
-bool
-HeRu::RuSpec::operator==(const RuSpec& other) const
-{
-    // we do not compare the RU PHY indices because they may be uninitialized for
-    // one of the compared RUs. This event should not cause the comparison to evaluate
-    // to false
-    return m_ruType == other.m_ruType && m_index == other.m_index &&
-           m_primary80MHz == other.m_primary80MHz;
-}
-
-bool
-HeRu::RuSpec::operator!=(const RuSpec& other) const
-{
-    return !(*this == other);
-}
-
-bool
-HeRu::RuSpec::operator<(const RuSpec& other) const
-{
-    // we do not compare the RU PHY indices because they may be uninitialized for
-    // one of the compared RUs. This event should not cause the comparison to evaluate
-    // to false
-    return std::tie(m_ruType, m_index, m_primary80MHz) <
-           std::tie(other.m_ruType, other.m_index, other.m_primary80MHz);
-}
-
 } // namespace ns3
