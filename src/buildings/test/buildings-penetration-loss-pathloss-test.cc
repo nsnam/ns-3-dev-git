aff661fada44aa3484556f106844fe94b1ad3cbd
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2025-10-28 20:32:46 +0100

    buildings: Fix streams for reliable testing across platforms

--- src/buildings/test/buildings-penetration-loss-pathloss-test.cc
+++ src/buildings/test/buildings-penetration-loss-pathloss-test.cc
@@ -16,6 +16,7 @@
 #include "ns3/log.h"
 #include "ns3/mobility-building-info.h"
 #include "ns3/mobility-helper.h"
+#include "ns3/rng-seed-manager.h"
 #include "ns3/simulator.h"
 #include "ns3/test.h"
 #include "ns3/three-gpp-propagation-loss-model.h"
@@ -114,75 +115,77 @@ BuildingsPenetrationLossesTestCase::DoRun()
     m_testVectors = {
         // PosA, PosB, B velocity, frequency, propagation model, O2I loss type, channel condition, mean and standard deviation of expected gaussian
         // 6GHz, pedestrian, Low O2I loss
-        {{0, 0, 35.0}, {  10, 0, 1.5},  0, 6e9, RMa,  O2ILH::LOW,  -93.0, 4.0}, // RMa O2I
-        {{0, 0, 35.0}, { 100, 0, 1.5},  0, 6e9, RMa,  O2ILH::LOW, -112.0, 4.0}, // RMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, RMa,  O2ILH::LOW,  -97.0, 1.0}, // RMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, RMa,  O2ILH::LOW, -110.0, 1.0}, // RMa O2O blocked
-        {{0, 0, 25.0}, {  10, 0, 1.5},  0, 6e9, UMa,  O2ILH::LOW, -101.0, 5.0}, // UMa O2I
-        {{0, 0, 25.0}, { 100, 0, 1.5},  0, 6e9, UMa,  O2ILH::LOW, -125.0, 5.0}, // UMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMa,  O2ILH::LOW,  -96.0, 1.0}, // UMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMa,  O2ILH::LOW, -117.0, 1.0}, // UMa O2O blocked
-        {{0, 0, 10.0}, {  10, 0, 1.5},  0, 6e9, UMi,  O2ILH::LOW,  -96.0, 5.0}, // UMi O2I
+        {{0, 0, 35.0}, {  10, 0, 1.5},  0, 6e9, RMa,  O2ILH::LOW,  -93.0, 4.5}, // RMa O2I
+        {{0, 0, 35.0}, { 100, 0, 1.5},  0, 6e9, RMa,  O2ILH::LOW, -112.0, 4.5}, // RMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, RMa,  O2ILH::LOW,  -97.0, 0.1}, // RMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, RMa,  O2ILH::LOW, -110.0, 0.1}, // RMa O2O blocked
+        {{0, 0, 25.0}, {  10, 0, 1.5},  0, 6e9, UMa,  O2ILH::LOW, -101.0, 5.3}, // UMa O2I
+        {{0, 0, 25.0}, { 100, 0, 1.5},  0, 6e9, UMa,  O2ILH::LOW, -125.0, 5.3}, // UMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMa,  O2ILH::LOW,  -96.0, 0.1}, // UMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMa,  O2ILH::LOW, -117.0, 0.1}, // UMa O2O blocked
+        {{0, 0, 10.0}, {  10, 0, 1.5},  0, 6e9, UMi,  O2ILH::LOW,  -96.0, 5.3}, // UMi O2I
         {{0, 0, 10.0}, { 100, 0, 1.5},  0, 6e9, UMi,  O2ILH::LOW, -127.0, 5.0}, // UMi O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMi,  O2ILH::LOW,  -99.0, 1.0}, // UMi O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMi,  O2ILH::LOW, -119.0, 1.0}, // UMi O2O blocked
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMi,  O2ILH::LOW,  -99.0, 0.1}, // UMi O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMi,  O2ILH::LOW, -119.0, 0.1}, // UMi O2O blocked
         // 6GHz, pedestrian, High O2I loss
-        {{0, 0, 35.0}, {  10, 0, 1.5},  0, 6e9, RMa, O2ILH::HIGH,  -93.0, 4.0}, // RMa O2I
-        {{0, 0, 35.0}, { 100, 0, 1.5},  0, 6e9, RMa, O2ILH::HIGH, -112.0, 4.0}, // RMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, RMa, O2ILH::HIGH,  -97.0, 1.0}, // RMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, RMa, O2ILH::HIGH, -110.0, 1.0}, // RMa O2O blocked
-        {{0, 0, 25.0}, {  10, 0, 1.5},  0, 6e9, UMa, O2ILH::HIGH, -101.0, 5.0}, // UMa O2I
-        {{0, 0, 25.0}, { 100, 0, 1.5},  0, 6e9, UMa, O2ILH::HIGH, -125.0, 5.0}, // UMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMa, O2ILH::HIGH,  -96.0, 1.0}, // UMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMa, O2ILH::HIGH, -117.0, 1.0}, // UMa O2O blocked
-        {{0, 0, 10.0}, {  10, 0, 1.5},  0, 6e9, UMi, O2ILH::HIGH,  -95.0, 5.0}, // UMi O2I
-        {{0, 0, 10.0}, { 100, 0, 1.5},  0, 6e9, UMi, O2ILH::HIGH, -127.0, 5.0}, // UMi O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMi, O2ILH::HIGH,  -99.0, 1.0}, // UMi O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMi, O2ILH::HIGH, -119.0, 1.0}, // UMi O2O blocked
+        {{0, 0, 35.0}, {  10, 0, 1.5},  0, 6e9, RMa, O2ILH::HIGH,  -93.0, 4.5}, // RMa O2I
+        {{0, 0, 35.0}, { 100, 0, 1.5},  0, 6e9, RMa, O2ILH::HIGH, -112.0, 4.5}, // RMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, RMa, O2ILH::HIGH,  -97.0, 0.1}, // RMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, RMa, O2ILH::HIGH, -110.0, 0.1}, // RMa O2O blocked
+        {{0, 0, 25.0}, {  10, 0, 1.5},  0, 6e9, UMa, O2ILH::HIGH, -101.0, 5.3}, // UMa O2I
+        {{0, 0, 25.0}, { 100, 0, 1.5},  0, 6e9, UMa, O2ILH::HIGH, -125.0, 5.3}, // UMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMa, O2ILH::HIGH,  -96.0, 0.1}, // UMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMa, O2ILH::HIGH, -117.0, 0.1}, // UMa O2O blocked
+        {{0, 0, 10.0}, {  10, 0, 1.5},  0, 6e9, UMi, O2ILH::HIGH,  -95.0, 5.3}, // UMi O2I
+        {{0, 0, 10.0}, { 100, 0, 1.5},  0, 6e9, UMi, O2ILH::HIGH, -127.0, 5.3}, // UMi O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 6e9, UMi, O2ILH::HIGH,  -99.0, 0.1}, // UMi O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 6e9, UMi, O2ILH::HIGH, -119.0, 0.1}, // UMi O2O blocked
         // 6GHz, vehicles, High O2I loss
-        {{0, 0, 35.0}, {  10, 0, 1.5}, 30, 6e9, RMa, O2ILH::HIGH,  -94.0, 4.0}, // RMa O2I
-        {{0, 0, 35.0}, { 100, 0, 1.5}, 30, 6e9, RMa, O2ILH::HIGH, -112.0, 4.0}, // RMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 6e9, RMa, O2ILH::HIGH, -106.0, 1.0}, // RMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 6e9, RMa, O2ILH::HIGH, -119.0, 1.0}, // RMa O2O blocked
-        {{0, 0, 25.0}, {  10, 0, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -101.0, 5.0}, // UMa O2I
-        {{0, 0, 25.0}, { 100, 0, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -125.0, 5.0}, // UMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -105.0, 5.0}, // UMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -126.0, 5.0}, // UMa O2O blocked
-        {{0, 0, 10.0}, {  10, 0, 1.5}, 30, 6e9, UMi, O2ILH::HIGH,  -95.0, 5.0}, // UMi O2I
-        {{0, 0, 10.0}, { 100, 0, 1.5}, 30, 6e9, UMi, O2ILH::HIGH, -127.0, 5.0}, // UMi O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 6e9, UMi, O2ILH::HIGH, -108.0, 5.0}, // UMi O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 6e9, UMi, O2ILH::HIGH, -128.0, 5.0}, // UMi O2O blocked
+        {{0, 0, 35.0}, {  10, 0, 1.5}, 30, 6e9, RMa, O2ILH::HIGH,  -94.0, 4.5}, // RMa O2I
+        {{0, 0, 35.0}, { 100, 0, 1.5}, 30, 6e9, RMa, O2ILH::HIGH, -112.0, 4.5}, // RMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 6e9, RMa, O2ILH::HIGH,  -98.0, 5.3}, // RMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 6e9, RMa, O2ILH::HIGH, -110.0, 5.3}, // RMa O2O blocked
+        {{0, 0, 25.0}, {  10, 0, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -101.0, 5.3}, // UMa O2I
+        {{0, 0, 25.0}, { 100, 0, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -125.0, 5.3}, // UMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 6e9, UMa, O2ILH::HIGH,  -96.0, 5.3}, // UMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 6e9, UMa, O2ILH::HIGH, -117.0, 5.3}, // UMa O2O blocked
+        {{0, 0, 10.0}, {  10, 0, 1.5}, 30, 6e9, UMi, O2ILH::HIGH,  -95.0, 5.3}, // UMi O2I
+        {{0, 0, 10.0}, { 100, 0, 1.5}, 30, 6e9, UMi, O2ILH::HIGH, -127.0, 5.3}, // UMi O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 6e9, UMi, O2ILH::HIGH, -100.0, 5.3}, // UMi O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 6e9, UMi, O2ILH::HIGH, -119.0, 5.3}, // UMi O2O blocked
         // Sub-6GHz, pedestrian, legacy O2I
-        {{0, 0, 35.0}, {  10, 0, 1.5},  0, 5e9, RMa, O2ILH::HIGH,  -97.0, 1.0}, // RMa O2I
-        {{0, 0, 35.0}, { 100, 0, 1.5},  0, 5e9, RMa, O2ILH::HIGH, -115.0, 1.0}, // RMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 5e9, RMa, O2ILH::HIGH,  -96.0, 1.0}, // RMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 5e9, RMa, O2ILH::HIGH, -108.0, 1.0}, // RMa O2O blocked
-        {{0, 0, 25.0}, {  10, 0, 1.5},  0, 5e9, UMa, O2ILH::HIGH, -108.0, 3.0}, // UMa O2I
-        {{0, 0, 25.0}, { 100, 0, 1.5},  0, 5e9, UMa, O2ILH::HIGH, -132.0, 3.0}, // UMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 5e9, UMa, O2ILH::HIGH,  -94.0, 1.0}, // UMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 5e9, UMa, O2ILH::HIGH, -117.0, 1.0}, // UMa O2O blocked
-        {{0, 0, 10.0}, {  10, 0, 1.5},  0, 5e9, UMi, O2ILH::HIGH, -103.0, 3.0}, // UMi O2I
-        {{0, 0, 10.0}, { 100, 0, 1.5},  0, 5e9, UMi, O2ILH::HIGH, -134.0, 3.0}, // UMi O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 5e9, UMi, O2ILH::HIGH,  -98.0, 1.0}, // UMi O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 5e9, UMi, O2ILH::HIGH, -119.0, 1.0}, // UMi O2O blocked
+        {{0, 0, 35.0}, {  10, 0, 1.5},  0, 5e9, RMa, O2ILH::HIGH,  -97.0, 0.1}, // RMa O2I
+        {{0, 0, 35.0}, { 100, 0, 1.5},  0, 5e9, RMa, O2ILH::HIGH, -115.0, 0.1}, // RMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 5e9, RMa, O2ILH::HIGH,  -96.0, 0.1}, // RMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 5e9, RMa, O2ILH::HIGH, -108.0, 0.1}, // RMa O2O blocked
+        {{0, 0, 25.0}, {  10, 0, 1.5},  0, 5e9, UMa, O2ILH::HIGH, -108.0, 3.6}, // UMa O2I
+        {{0, 0, 25.0}, { 100, 0, 1.5},  0, 5e9, UMa, O2ILH::HIGH, -132.0, 3.6}, // UMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 5e9, UMa, O2ILH::HIGH,  -94.0, 0.1}, // UMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 5e9, UMa, O2ILH::HIGH, -117.0, 0.1}, // UMa O2O blocked
+        {{0, 0, 10.0}, {  10, 0, 1.5},  0, 5e9, UMi, O2ILH::HIGH, -103.0, 3.6}, // UMi O2I
+        {{0, 0, 10.0}, { 100, 0, 1.5},  0, 5e9, UMi, O2ILH::HIGH, -134.0, 3.6}, // UMi O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5},  0, 5e9, UMi, O2ILH::HIGH,  -98.0, 0.1}, // UMi O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5},  0, 5e9, UMi, O2ILH::HIGH, -119.0, 0.1}, // UMi O2O blocked
         // Sub-6GHz, vehicles, legacy O2I
-        {{0, 0, 35.0}, {  10, 0, 1.5}, 30, 5e9, RMa, O2ILH::HIGH,  -97.0, 1.0}, // RMa O2I
-        {{0, 0, 35.0}, { 100, 0, 1.5}, 30, 5e9, RMa, O2ILH::HIGH, -115.0, 1.0}, // RMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 5e9, RMa, O2ILH::HIGH, -104.0, 1.0}, // RMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 5e9, RMa, O2ILH::HIGH, -117.0, 1.0}, // RMa O2O blocked
-        {{0, 0, 25.0}, {  10, 0, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -108.0, 1.0}, // UMa O2I
-        {{0, 0, 25.0}, { 100, 0, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -132.0, 1.0}, // UMa O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -104.0, 1.0}, // UMa O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -126.0, 1.0}, // UMa O2O blocked
-        {{0, 0, 10.0}, {  10, 0, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -102.0, 1.0}, // UMi O2I
-        {{0, 0, 10.0}, { 100, 0, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -134.0, 1.0}, // UMi O2I
-        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -106.0, 1.0}, // UMi O2O unblocked
-        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -128.0, 1.0}, // UMi O2O blocked
+        {{0, 0, 35.0}, {  10, 0, 1.5}, 30, 5e9, RMa, O2ILH::HIGH,  -97.0, 0.1}, // RMa O2I
+        {{0, 0, 35.0}, { 100, 0, 1.5}, 30, 5e9, RMa, O2ILH::HIGH, -115.0, 0.1}, // RMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 5e9, RMa, O2ILH::HIGH,  -96.0, 5.0}, // RMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 5e9, RMa, O2ILH::HIGH, -108.0, 5.0}, // RMa O2O blocked
+        {{0, 0, 25.0}, {  10, 0, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -108.0, 3.6}, // UMa O2I
+        {{0, 0, 25.0}, { 100, 0, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -132.0, 3.6}, // UMa O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -103.0, 5.0}, // UMa O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 5e9, UMa, O2ILH::HIGH, -117.0, 5.0}, // UMa O2O blocked
+        {{0, 0, 10.0}, {  10, 0, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -102.0, 3.6}, // UMi O2I
+        {{0, 0, 10.0}, { 100, 0, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -134.0, 3.6}, // UMi O2I
+        {{0, 0, 10.0}, { 50, 15, 1.5}, 30, 5e9, UMi, O2ILH::HIGH,  -98.0, 5.0}, // UMi O2O unblocked
+        {{0, 0, 10.0}, {1000, 0, 1.5}, 30, 5e9, UMi, O2ILH::HIGH, -119.0, 5.0}, // UMi O2O blocked
     };
     // clang-format on
-
+    RngSeedManager::SetSeed(1);
+    RngSeedManager::SetRun(1);
     for (std::size_t i = 0; i < m_testVectors.size(); i++)
     {
+        RngSeedManager::ResetNextStreamIndex();
         // create the factory for the propagation loss model
         ObjectFactory propModelFactory;
 
@@ -215,12 +218,13 @@ BuildingsPenetrationLossesTestCase::DoRun()
         buildingInfoB->MakeConsistent(b);
 
         Ptr<ChannelConditionModel> condModel = CreateObject<BuildingsChannelConditionModel>();
+        auto streamOffset = condModel->AssignStreams(0);
 
         // Configure test case setup
         const auto& testVector = m_testVectors.at(i);
         a->SetPosition(testVector.m_positionA);
         b->SetPosition(testVector.m_positionB);
-        bcv->SetVelocity({testVector.m_ueVelocity, 0, 0});
+        bcv->SetVelocity({testVector.m_ueVelocity / 3.6, 0, 0});
         bool isAIndoor = buildingInfoA->IsIndoor();
         bool isBIndoor = buildingInfoB->IsIndoor();
 
@@ -239,10 +243,12 @@ BuildingsPenetrationLossesTestCase::DoRun()
         m_propModel = propModelFactory.Create<ThreeGppPropagationLossModel>();
         m_propModel->SetAttribute("Frequency", DoubleValue(testVector.m_frequency));
         m_propModel->SetAttribute("ShadowingEnabled", BooleanValue(false));
+        streamOffset = m_propModel->AssignStreams(streamOffset);
+        streamOffset = m_propModel->GetChannelConditionModel()->AssignStreams(streamOffset);
         m_propModel->SetChannelConditionModel(condModel);
-        m_propModel->AssignStreams(0);
-
-        std::vector<double> samples(10000);
+        streamOffset = m_propModel->AssignStreams(streamOffset);
+        m_propModel->GetChannelConditionModel()->AssignStreams(streamOffset);
+        std::vector<double> samples(1000);
         for (auto& sample : samples)
         {
             // Compute total loss
@@ -251,7 +257,7 @@ BuildingsPenetrationLossesTestCase::DoRun()
             m_propModel->ClearO2iLossCacheMap();
         }
 
-        auto getMeanAndStddev = [](const std::vector<double>& vec) {
+        auto getMeanAndStddev = [](const std::vector<double> vec) {
             // Calculate the mean
             double sum = std::accumulate(vec.begin(), vec.end(), 0.0);
             double mean = sum / vec.size();
@@ -262,41 +268,16 @@ BuildingsPenetrationLossesTestCase::DoRun()
             return std::make_pair(mean, stddev);
         };
 
-        auto chi2 = [](std::vector<double>& vecTested,
-                       std::vector<double>& vecReference) -> const double {
-            std::sort(vecTested.begin(), vecTested.end());
-            std::sort(vecReference.begin(), vecReference.end());
-            double chi2 = 0.0;
-            for (std::size_t i = 0; i < vecTested.size(); ++i)
-            {
-                if (vecReference[i] == 0)
-                {
-                    throw std::domain_error("Expected value at index " + std::to_string(i) +
-                                            " is zero");
-                }
-                double diff = vecTested[i] - vecReference[i];
-                chi2 += (diff * diff) / vecReference[i];
-            }
-            chi2 /= vecTested.size();
-            NS_ABORT_MSG_IF(std::isnan(chi2), "Chi-squared is NaN");
-            return chi2;
-        };
-        Ptr<NormalRandomVariable> n = CreateObject<NormalRandomVariable>();
-        n->SetStdDev(testVector.m_expectedStddev);
-        n->SetAttribute("Mean", DoubleValue(testVector.m_expectedMean));
-        std::vector<double> reference(10000);
-        std::transform(reference.begin(), reference.end(), reference.begin(), [n](auto& a) {
-            return a = n->GetValue();
-        });
-        auto chi = chi2(samples, reference);
         auto [mean, stddev] = getMeanAndStddev(samples);
-        NS_TEST_ASSERT_MSG_LT_OR_EQ(std::abs(chi),
-                                    0.5,
-                                    "Test vector "
-                                        << i << ", expected mean=" << testVector.m_expectedMean
-                                        << " and stddev=" << testVector.m_expectedStddev
-                                        << ", got mean=" << mean << " and stddev=" << stddev
-                                        << ". Chi-squared is higher than threshold: " << chi);
+        NS_TEST_ASSERT_MSG_EQ_TOL(mean,
+                                  testVector.m_expectedMean,
+                                  -testVector.m_expectedMean * 0.1,
+                                  "Test vector " << i << ", mean outside expected range");
+        NS_TEST_ASSERT_MSG_EQ_TOL(stddev,
+                                  testVector.m_expectedStddev,
+                                  std::max(0.1, testVector.m_expectedStddev * 0.1),
+                                  "Test vector " << i
+                                                 << ", standard deviation outside expected range");
         m_propModel = nullptr;
         Simulator::Destroy();
     }
